39ad6ccaf6a512a0d47bf1067f02b4e2bd3657a7
WilliamSotaquira <william.sotaquira@gmail.com>
2022-07-13 00:30:34 -0500

    Creación de pagina web para informes de las Ciclovias Temporales en Bogotá

---
+++ resources/views/2022/07/cicliviasTemporales.blade.php
@@ -0,0 +1,155 @@
+
+<!-- ----- ----- ----- ----- ----- ----- ----- ----- Bloque Seccion ----- ----- ----- ----- ----- ----- ----- ----- ----- ------ -->
+<div class="container-fluid seccion">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="encabezado">
+                <h3>Informes de Ciclovías Temporales</h3>
+            </div>
+            <hr>
+            <div class="descripcion">
+                <p>La Secretaría Distrital de Movilidad pública los informes periódicos y específicos con información del seguimiento a los corredores de ciclovías temporales, dando cumplimiento del Decreto 073 de 2021 expedido el 16 de marzo de 2021 por la Alcaldía de Bogotá</p>
+            </div>
+            <hr>
+        </div>
+    </div>
+
+    <div class="row">
+        <!-- collapse -->
+   
+        <!-- collapse_informesCiclovias -->
+        <div class='panel-group' id='accordion_informesCiclovias_informesCiclovias' role='tablist ' aria-multiselectable='true'>
+
+        <!-- collapse_2022 -->
+        <div class='panel panel-default'>
+            <div class='panel-heading ph1' role='tab' id='heading_2022'>
+                <h4 class='panel-title'>
+                    <a role='button' data-toggle='collapse' data-parent='#accordion_informesCiclovias' href='#collapse_2022' aria-expanded='true' aria-controls='collapse_2022'>
+                        2022
+                    </a>
+                </h4>
+            </div>
+            <div id='collapse_2022' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading_2022'>
+                <div class='panel-body'>
+                    <ul>
+                        <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/12-07-2022/informe_cl68-72.pdf" target="_blank" rel="noopener noreferrer">Ciclovía temporal Calle 68 - 72</a></li>
+                        <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/12-07-2022/ciclovias_marzo_2022.pdf" target="_blank" rel="noopener noreferrer">Ciclovías temporales - Balance Enero – Marzo de 2022</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /#fin _2022 -->
+        
+        <!-- collapse_2021 -->
+        <div class='panel panel-default'>
+            <div class='panel-heading ph1' role='tab' id='heading_2021'>
+                <h4 class='panel-title'>
+                    <a role='button' data-toggle='collapse' data-parent='#accordion_informesCiclovias' href='#collapse_2021' aria-expanded='true' aria-controls='collapse_2021'>
+                        2021
+                    </a>
+                </h4>
+            </div>
+            <div id='collapse_2021' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading_2021'>
+                <div class='panel-body'>
+                    <ul>
+                        <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/12-07-2022/ciclovias_dic_2021.pdf" target="_blank" rel="noopener noreferrer">Ciclovías temporales - Balance a Diciembre de 2021</a></li>
+                        <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/12-07-2022/ciclovias_sep_2021.pdf" target="_blank" rel="noopener noreferrer">Ciclovías temporales - Balance a Septiembre de 2021</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /#fin _2021 -->
+        
+        </div>
+        <!-- /#fin _informesCiclovias -->
+    
+        <!-- /#fin  -->
+    </div>
+</div>
+<!-- ----- ----- ----- ----- ----- ----- ----- -----  Fin Bloque Sección  ----- ----- ----- ----- ----- ----- ----- ----- ------ -->
+<!-- ----- ----- ----- ----- ----- ----- ----- -----  Hoja de estilo ---- ----- ----- ----- ----- ----- ----- ----- ----- ------ -->
+<style type="text/css">
+    .field-item.even h2 {
+        visibility: hidden;
+        margin-top: -50px;
+    }
+
+    .region-content {
+        padding: 0px;
+        overflow: hidden;
+    }
+
+    .seccion {
+       font-size: 15px;
+       font-weight: 500;
+       text-align: justify;
+       line-height: 1.4;
+       color: rgba(25, 25, 25, 1);
+       margin: 8px;
+    }
+
+    .w-100 {
+        width: 100%;
+    }
+
+    .seccion .encabezado {
+        text-align: center;
+    }
+
+    .ph1 {
+        background-color: #4D541F !important;
+
+    }
+
+    .ph1 a {
+        color: #FFFFFF !important;
+        font-size: 15px;
+    }
+
+    .ph1 a:hover {
+        color: #c9d64f !important;
+    }
+
+    .ph2 {
+        background-color: #909D2A !important;
+    }
+
+    .ph2 a {
+        color: #000 !important;
+    }
+
+    .ph2 a:hover {
+        color: #4D541F !important;
+    }
+
+    .ph2 {
+        background-color: #A6B517 !important;
+    }
+
+    .ph2 a {
+        color: #212121 !important;
+    }
+
+    .ph2 a:hover {
+        color: #4D541F !important;
+    }
+
+    .panel-body {
+        font-size: 15px !important;
+        background-color: #F0F2D9 !important;
+    }
+
+    .seccion ul li {
+        font-size: 15px !important;
+    }
+    .seccion ul li a{
+        color: rgba(25, 25, 25, 1);
+    }
+    .seccion ul li a:hover{
+        color: #4D541F;
+        font-weight: 600;
+    }
+
+
+</style>
+<!-- ----- ----- ----- ----- ----- ----- ----- -----  Fin hoja de estilo  ----- ----- ----- ----- ----- ----- ----- ----- ------ -->
