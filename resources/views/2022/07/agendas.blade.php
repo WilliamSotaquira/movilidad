32b669cd1121000169c2d093e8ded4db4571fe58
WilliamSotaquira <william.sotaquira@gmail.com>
2022-07-28 06:59:23 -0500

    Actualización a las 36 agendas de los directivos en estructura y forma

---
+++ resources/views/2022/07/agendas.blade.php
@@ -0,0 +1,26 @@
+<h4 style='text-align: center;'>Ana María Corredor Yunis</h4>
+<hr>
+<iframe src='https://calendar.google.com/calendar/b/4/embed?height=600&amp;ctz=America%2FBogota&amp;src=acorredor%40movilidadbogota.gov.co&amp;title=Agenda%20de%20la%20OACCM&amp;wkst=1&amp;mode=AGENDA&amp;bgcolor=%234d541f&amp;color=%23C9D64F&amp;showTabs=0&amp;showPrint=0&amp;showDate=0&amp;showTz=0&amp;showNav=1&amp;showTitle=0' style='border:solid 1px #777' width='100%' height='600' frameborder='0' scrolling='no'></iframe>
+
+
+
+
+<div class='video'>
+    <div class='embed-responsive embed-responsive-16by9 text-center'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/XXH84THD9aM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</div>
+<div class='video'>
+    <div class='embed-responsive embed-responsive-16by9 text-center'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/5gO_8TGxkz8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</div>
+
+Este sábado 23 de julio entra en operación el Par Vial Suba Tibabuyes 
+
+<style>
+    .video{
+        max-width: 70%;
+        margin: auto;
+    }
+</style>
