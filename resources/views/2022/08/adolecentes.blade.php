52ae773863f805108ffb4db750c814459b9743f9
WilliamSotaquira <william.sotaquira@gmail.com>
2022-08-30 00:50:06 -0500

    Creación de la pagina web niños, niñas y adolecentes

---
+++ resources/views/2022/08/adolecentes.blade.php
@@ -0,0 +1,49 @@
+<style>
+    .nna .panel-default>.panel-heading {
+        background-color: rgba(77, 84, 31, 1) !important;
+        /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#2a611f+0,4d541f+30,4d541f+70,2a611f+100 */
+        background: rgb(42, 97, 31);
+        /* Old browsers */
+        background: -moz-linear-gradient(top, rgba(42, 97, 31, 1) 0%, rgba(77, 84, 31, 1) 30%, rgba(77, 84, 31, 1) 70%, rgba(42, 97, 31, 1) 100%);
+        /* FF3.6-15 */
+        background: -webkit-linear-gradient(top, rgba(42, 97, 31, 1) 0%, rgba(77, 84, 31, 1) 30%, rgba(77, 84, 31, 1) 70%, rgba(42, 97, 31, 1) 100%);
+        /* Chrome10-25,Safari5.1-6 */
+        background: linear-gradient(to bottom, rgba(42, 97, 31, 1) 0%, rgba(77, 84, 31, 1) 30%, rgba(77, 84, 31, 1) 70%, rgba(42, 97, 31, 1) 100%);
+        /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#2a611f', endColorstr='#2a611f', GradientType=0);
+        /* IE6-9 */
+    }
+
+    .nna .panel-default>.panel-heading h4>a {
+        /* font-size: 1em; */
+        font-weight: 700;
+        text-align: left;
+        line-height: 1.4;
+        color: rgba(255, 255, 255, 1);
+        margin: 8px;
+    }
+</style>
+
+<div class="nna">
+    <hr>
+    <!-- collapse_matriz-nna -->
+    <div class='panel panel-default'>
+        <div class='panel-heading' role='tab' id='heading_matriz-nna'>
+            <h4 class='panel-title'>
+                <a role='button' data-toggle='collapse' data-parent='#accordion' href='#collapse_matriz-nna' aria-expanded='true' aria-controls='collapse_matriz-nna'>
+                    Matriz Niños, Niñas y Adolescentes
+                </a>
+            </h4>
+        </div>
+        <div id='collapse_matriz-nna' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_matriz-nna'>
+            <div class='panel-body'>
+                <ul>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/25-08-2022/matriz_ninos_ninas_y_adolescentes_v1-_28_jul_2022.xlsx" target="_blank" rel="noopener noreferrer">Matriz niños, niñas y adolescentes - julio 2022 .xlsx</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/28-12-2021/ninos_ninas_y_adolescentes_julio_-_diciembre_2021_web.xlsx">Matriz niños, niñas y adolescentes - diciembre 2022 .xlsx</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /#fin _matriz-nna -->
+
+</div>
