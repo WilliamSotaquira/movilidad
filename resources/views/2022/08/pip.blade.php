3760855706b349d4cb717109bf0e1c757ab7fa6d
WilliamSotaquira <william.sotaquira@gmail.com>
2022-08-30 01:03:03 -0500

    Creación de sitio web Plan Institucional De Participación

---
+++ resources/views/2022/08/pip.blade.php
@@ -0,0 +1,114 @@
+            <!-- Sección Plan Institucional De Participación – PIP -->
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h4 class="panel-title"><a data-parent="#accordionPRIN" data-toggle="collapse" href="#collapsePRIN1">Plan Institucional De Participación – PIP</a></h4>
+				</div>
+
+				<div class="panel-collapse collapse" id="collapsePRIN1">
+					<div class="panel-body">
+						<div class="panel-group" id="accordionPIP"><!-- collapse_pip2022 -->
+							<div class="panel panel-primary">
+								<div class="panel-heading" id="heading_pip2022" role="tab">
+									<h4 class="panel-title"><a aria-controls="collapse_pip2022" aria-expanded="true" data-parent="#accordionPIP" data-toggle="collapse" href="#collapse_pip2022" role="button">PIP 2022 </a></h4>
+								</div>
+
+								<div aria-labelledby="heading_pip2022" class="panel-collapse collapse in" id="collapse_pip2022" role="tabpanel">
+									<div class="panel-body">
+										<ul>
+											<li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/15-02-2022/m06-pl01_plan_institucional_de_participacion-sdm_v2.0_15-02-2022.pdf">PIP 2022 - v.2.0</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /#fin _pip2022 -->
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a data-parent="#accordionPIP" data-toggle="collapse" href="#collapse2021">PIP 2021</a></h4>
+								</div>
+
+								<div class="panel-collapse collapse" id="collapse2021">
+									<div class="panel-body">
+										<ul>
+											<li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/08-04-2021/210406_plan_institucional_de_participacion_2021_v_2.0.pdf">PIP 2021 -&nbsp;v.2.0</a>&nbsp;&nbsp;</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a data-parent="#accordionPIP" data-toggle="collapse" href="#collapse2020">PIP 2020</a></h4>
+								</div>
+
+								<div class="panel-collapse collapse" id="collapse2020">
+									<div class="panel-body">
+										<ul>
+											<li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/05-11-2020/plan_institucional_de_participacion_ciudadana_2020.pdf">PIP 2020</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a data-parent="#accordionPIP" data-toggle="collapse" href="#collapse2019-2020">PIP 2019</a></h4>
+								</div>
+
+								<div class="panel-collapse collapse" id="collapse2019-2020">
+									<div class="panel-body">
+										<ul>
+											<li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-12-2019/pip_final_2019.pdf">PIP 2019</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a data-parent="#accordionPIP" data-toggle="collapse" href="#collapsePIP1">PIP 2018-2019</a></h4>
+								</div>
+
+								<div class="panel-collapse collapse" id="collapsePIP1">
+									<div class="panel-body">
+										<ul>
+											<li><a href="http://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/2018-11-28/PLAN%20INSTITUCIONAL%20DE%20PARTICIPACI%C3%93N%20PIP%202018-2019%20DE%2022-11-2018.pdf">PM05-PR02-MD01 Plan Institucional de Participación</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a data-parent="#accordionPIP" data-toggle="collapse" href="#collapsePIP2">PIP 2017-2018</a></h4>
+								</div>
+
+								<div class="panel-collapse collapse" id="collapsePIP2">
+									<div class="panel-body">
+										<ul>
+											<li><a href="http://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/2018-09-04/PM05-PR02-F04%20PLAN%20INSTITUCIONAL%20DE%20PARTICIPACI%C3%93N%202017-2018.pdf">PM05-PR02-MD01 Plan Institucional de Participación</a></li>
+											<li><a href="http://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/2018-09-04/PM05-PR02-PT01%20PROTOCOLO%20PARTICIPACI%C3%93N%20CIUDADANA%20LOCAL%20V3.0.pdf" target="_blank">PM05-PR02-PT01 Protocolo de Participación Ciudadana Local</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="panel panel-primary">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a data-parent="#accordionPIP" data-toggle="collapse" href="#collapsePIP3">PIP 2016-2017</a></h4>
+								</div>
+
+								<div class="panel-collapse collapse" id="collapsePIP3">
+									<div class="panel-body">
+										<ul>
+											<li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/2018-11-13/PM05-PR02-PT01%20PROTOCOLO%20PARTICIPACI%C3%93N%20CIUDADANA%20LOCAL%20TEMAS%20DE%20MOVILIDAD%20V2.pdf" target="_blank">PM05-PR02-PT01 Protocolo de Participación Ciudadana</a></li>
+											<li><a href="https://drive.google.com/file/d/0ByNoeWkPXuHpcTk4bzB5S1JoOGc/view?usp=sharing" target="_blank">PM05-PR02-F04 Plan Institucional de Participación - PIP</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Fin Sección --><!-- Sección Rendición de Cuentas Locales -->
