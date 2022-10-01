24eee5f8bf81d95033a7941506a0af19085cb2b1
WilliamSotaquira <william.sotaquira@gmail.com>
2022-10-01 09:15:25 -0500

    Actualización en la sección principal de avisos de la pagina web de la Entidad

--- resources/views/inicio/avisos.blade.php
+++ resources/views/inicio/avisos.blade.php
@@ -3,21 +3,36 @@
 
 <!-- Sección Bloque de Mantenimiento  -->
 <div class="box-avisos">
-    
+
     <hr>
-    <div class="row">
-        <!-- banner-xs  -->
-        <!-- <div class="col-xs-12 visible-xs-12 visible-xs-block">
+    <!-- <div class="row"> -->
+    <!-- banner-xs  -->
+    <!-- <div class="col-xs-12 visible-xs-12 visible-xs-block">
             <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-08-2022/-_benner_900x300.jpg" title="Aviso Mantenimiento Sistema Contravencional">
         </div> -->
-        <!-- Fin banner-xs  -->
-        
-        <!-- banner-lg  -->
-        <!-- <div class="col-sm-12 hidden-xs">
+    <!-- Fin banner-xs  -->
+
+    <!-- banner-lg  -->
+    <!-- <div class="col-sm-12 hidden-xs">
             <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-08-2022/-_banner_1920x640.jpg" title="Aviso Mantenimiento Sistema Contravencional">
         </div> -->
-        <!-- Fin banner-lg  -->
+    <!-- Fin banner-lg  -->
+    <!-- </div> -->
+
+    <div class="row">
+        <div class="col-xs-12 visible-xs-12 visible-xs-block">
+            <a href="https://www.movilidadbogota.gov.co/web/semana_de_la_bici_2022" target="_blank" rel="noopener noreferrer">
+                <img alt="Click aquí para acceder " class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/19-09-2022/semanabici_900x300.png" title="Banner Semana de la Bici - 2022">
+            </a>
+        </div>
+        <div class="col-sm-12 hidden-xs">
+            <a href="https://www.movilidadbogota.gov.co/web/semana_de_la_bici_2022" target="_blank" rel="noopener noreferrer">
+                <img alt="Click aquí para acceder " class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/19-09-2022/semanabici_1920_x320.png" title="Banner Semana de la Bici - 2022">
+            </a>
+        </div>
     </div>
+
+    <hr>
 
 </div>
 <!-- Fin Sección Bloque de Mantenimiento  -->
