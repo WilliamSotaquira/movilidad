dd5b4a7a67f8943df1059714a43ada9c18cd4847
WilliamSotaquira <william.sotaquira@gmail.com>
2022-10-27 13:38:30 -0500

    Creación de la pagina web "Esquema de publicación SDM" para la sección de Transparencia y en cumplimiento a la Resolución 1519 de 2020

---
+++ resources/views/2022/10/esquema.blade.php
@@ -0,0 +1,191 @@
+@extends('welcome')
+@section('contenido')
+
+<!-- Set Wrapper -->
+<div class='container-fluid'>
+    <div class='box-wrp'>
+
+    <hr>
+<p>Este esquema presenta toda la información alojada en la página para conocimiento y acceso público, describiendo entre otras características el formato del documento, la fecha de generación del mismo, su frecuencia de actualización, y el área o dependencia responsable de su producción y actualización. La Secretaría Distrital de Movilidad hace consolidación de esta información mensualmente.</p>
+<br>
+
+<!-- collapse_esquema -->
+<div class='panel-group' id='accordion_esquema' role='tablist ' aria-multiselectable='true'>
+
+    <!-- collapse-publicacionesMinimas -->
+    <div class='panel panel-primary'>
+        <div class='panel-heading' role='tab' id='heading-publicacionesMinimas'>
+            <h4 class='panel-title'>
+                <a role='button' data-toggle='collapse' data-parent='#accordion_esquema' href='#collapse-publicacionesMinimas' aria-expanded='true' aria-controls='collapse-publicacionesMinimas'>
+                    Matriz de Cumplimiento de las Publicaciones Mínimas
+                </a>
+            </h4>
+        </div>
+        <div id='collapse-publicacionesMinimas' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading-publicacionesMinimas'>
+            <div class='panel-body'>
+
+                <!-- collapse_minimas22 -->
+                <div class='panel panel-primary'>
+                    <div class='panel-heading' role='tab' id='heading_minimas22'>
+                        <h4 class='panel-title'>
+                            <a role='button' data-toggle='collapse' data-parent='#collapse-publicacionesMinimas' href='#collapse_minimas22' aria-expanded='true' aria-controls='collapse_minimas22'>
+                                2022
+                            </a>
+                        </h4>
+                    </div>
+                    <div id='collapse_minimas22' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading_minimas22'>
+                        <div class='panel-body'>
+
+                            <ul>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/14-10-2022/matriz_de_cumplimiento_de_las_publicaciones_minimas_-_octubre_2022.xlsx_-_hoja1.pdf" target="_blank" rel="noopener noreferrer">octubre</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- /#fin _minimas22 -->
+
+            </div>
+        </div>
+    </div>
+    <!-- /#fin -publicacionesMinimas -->
+
+
+    <!-- collapse_transparencia -->
+    <div class='panel panel-primary'>
+        <div class='panel-heading' role='tab' id='heading_transparencia'>
+            <h4 class='panel-title'>
+                <a role='button' data-toggle='collapse' data-parent='#accordion_esquema' href='#collapse_transparencia' aria-expanded='true' aria-controls='collapse_transparencia'>
+                    Esquema de Publicación de Información
+                </a>
+            </h4>
+        </div>
+        <div id='collapse_transparencia' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading_transparencia'>
+            <div class='panel-body'>
+
+<!-- collapse_grupoTransparencia -->
+<div class='panel-group' id='accordion_grupoTransparencia' role='tablist ' aria-multiselectable='true'>
+
+      <!-- collapse_esuquema22 -->
+                <div class='panel panel-primary'>
+                    <div class='panel-heading' role='tab' id='heading_esuquema22'>
+                        <h4 class='panel-title'>
+                            <a role='button' data-toggle='collapse' data-parent='#accordion_grupoTransparencia' href='#collapse_esuquema22' aria-expanded='true' aria-controls='collapse_esuquema22'>
+                                2022
+                            </a>
+                        </h4>
+                    </div>
+                    <div id='collapse_esuquema22' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading_esuquema22'>
+                        <div class='panel-body'>
+                            <ul>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/14-10-2022/matriz_de_cumplimiento_de_la_politica_de_transparencia_y_acceso_a_la_informacion_publica_-_octubre_2022.xlsx_-_matriz.pdf" target="_blank" rel="noopener noreferrer">octubre</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/01-10-2022/registro_de_publiaciones_-_septiembre_2022.xlsx" target="_blank" rel="noopener noreferrer">septiembre</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/01-10-2022/registro_de_publiaciones_-_agosto_2022.xlsx" target="_blank" rel="noopener noreferrer">agosto</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/22-08-2022/matriz_de_cumplimiento_de_la_politica_de_transparencia_y_acceso_a_la_informacion_publica_-_junio_2022.xlsx" target="_blank" rel="noopener noreferrer">junio</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/22-08-2022/matriz_de_cumplimiento_de_la_politica_de_transparencia_y_acceso_a_la_informacion_publica_-_junio_2022.xlsx" target="_blank" rel="noopener noreferrer">junio</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-12-2021/20211223_matriz_procedimiento_y_publicacion.xlsx" target="_blank" rel="noopener noreferrer">enero</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- /#fin _esuquema22 -->
+
+                <!-- collapse_esquema21 -->
+                <div class='panel panel-primary'>
+                    <div class='panel-heading' role='tab' id='heading_esquema21'>
+                        <h4 class='panel-title'>
+                            <a role='button' data-toggle='collapse' data-parent='#accordion_grupoTransparencia' href='#collapse_esquema21' aria-expanded='true' aria-controls='collapse_esquema21'>
+                                2021
+                            </a>
+                        </h4>
+                    </div>
+                    <div id='collapse_esquema21' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading_esquema21'>
+                        <div class='panel-body'>
+                            <ul>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-12-2021/20211223_matriz_procedimiento_y_publicacion.xlsx" target="_blank" rel="noopener noreferrer">noviembre</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-06-2021/esquema_de_publicacion_de_informacion_de_la_seccion_de_ley_de_transparencia_-_20210622_-_web.xlsx" target="_blank" rel="noopener noreferrer">junio</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/18-01-2021/esquema_de_publicacion_de_informacion_de_la_seccion_de_ley_de_transparencia_-_20210118_-_web.xlsx" target="_blank" rel="noopener noreferrer">enero</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- /#fin _esquema21 -->
+
+                <!-- collapse_esquema2020 -->
+                <div class='panel panel-primary'>
+                    <div class='panel-heading' role='tab' id='heading_esquema2020'>
+                        <h4 class='panel-title'>
+                            <a role='button' data-toggle='collapse' data-parent='#accordion_grupoTransparencia' href='#collapse_esquema2020' aria-expanded='true' aria-controls='collapse_esquema2020'>
+                                2020
+                            </a>
+                        </h4>
+                    </div>
+                    <div id='collapse_esquema2020' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading_esquema2020'>
+                        <div class='panel-body'>
+                            <ul>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/24-02-2020/esquema_de_publicacion_de_informacion_de_la_seccion_de_ley_de_transparencia_-_20200224_-_web.xlsx" target="_blank" rel="noopener noreferrer">febrero</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/05-04-2020/esquema_de_publicacion_de_informacion_de_la_seccion_de_ley_de_transparencia_-_20200403_-_web.xlsx" target="_blank" rel="noopener noreferrer">abril</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/02-06-2020/esquema_de_publicacion_de_informacion_de_la_seccion_de_ley_de_transparencia_-_20200602_-_web_0.xlsx" target="_blank" rel="noopener noreferrer">junio</a></li>
+                                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/01-09-2020/esquema_de_publicacion_de_informacion_de_la_seccion_de_ley_de_transparencia_-_20200901_-_web.xlsx" target="_blank" rel="noopener noreferrer">septiembre</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- /#fin _esquema2020 -->
+</div>
+<!-- /#fin _grupoTransparencia -->
+
+
+
+
+            </div>
+        </div>
+    </div>
+    <!-- /#fin _transparencia -->
+
+</div>
+<!-- /#fin _esquema -->
+
+
+
+    </div>
+</div>
+<!-- End Set Wrapper -->
+
+<!-- Styles -->
+<style type='text/css'>
+    .field-item.even h2 {
+        visibility: hidden;
+        margin-top: -50px;
+    }
+
+    .region-content {
+        padding: 0px;
+        overflow:hidden;
+    }
+
+     {
+        max-width: 980px;
+        margin: auto;
+        word-break: break-all;
+        word-wrap: break-word;
+
+    }
+
+/*
+     [class*='col-'] {
+        margin: 0px;
+        padding: 0px;
+        border: 0;
+        outline: 0;
+        font-size: 100%;
+        vertical-align: baseline;
+    }
+    */
+
+    .w-100 {
+        width: 100%;
+    }
+
+</style>
+<!-- End Styles -->
