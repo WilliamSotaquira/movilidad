cb0f04db8600b5203b5a4552e4739b6ee38e8e5d
WilliamSotaquira <william.sotaquira@gmail.com>
2022-10-27 13:41:15 -0500

    Creación y unificación de información en la pagina web "Memorias Ferias de Servicio - SDM"

---
+++ resources/views/2022/10/ferias.blade.php
@@ -0,0 +1,48 @@
+<hr>
+<p>Es la estrategia de atención móvil que articula a las diferentes entidades, del orden distrital, nacional y privado que ejercen funciones públicas para la prestación de un servicio integral en el ámbito local, donde se ofrece a la ciudadanía información y acceso a programas y trámites, prestando un servicio más amable y efectivo, en diferentes zonas de la ciudad.</p>
+<br>
+<!-- collapse ferias de servicio -->
+<div class='panel-group' id='accordion_ferias' role='tablist ' aria-multiselectable='true'>
+
+    <!-- collapse_2022 -->
+    <div class='panel panel-primary'>
+        <div class='panel-heading' role='tab' id='heading_2022'>
+            <h4 class='panel-title'>
+                <a role='button' data-toggle='collapse' data-parent='#accordion' href='#collapse_2022' aria-expanded='true' aria-controls='collapse_2022'>
+                    2022
+                </a>
+            </h4>
+        </div>
+        <div id='collapse_2022' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_2022'>
+            <div class='panel-body'>
+
+                <ul>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/14-02-2022/memoria_feria_de_servicios_localidad_engativa_2.pdf">Memoria Feria de Servicios Localidad Engativa</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/14-02-2022/memoria_feria_de_servicios_localidad_kennedy_2.pdf">Memoria Feria de Servicios Localidad Kennedy</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/14-02-2022/memoria_feria_de_servicios_localidad_ciudad_bolivar.pdf">Memoria Feria de Servicios Localidad Ciudad Bolívar</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/18-02-2022/memoria_feria_de_servicios_localidad_bosa_1.pdf">Memoria Feria de Servicios Localidad Bosa</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/03-03-2022/memoria_feria_de_servicios_localidad_fontibon.pdf">Memoria Feria de Servicios Localidad de Fontibón</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/18-03-2022/memoria_feria_de_servicios_localidad_chapinero.pdf">Memoria Feria de Servicios Localidad Chapinero</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/18-03-2022/memoria_feria_de_servicios_localidad_suba.pdf">Memoria Feria de Servicios Localidad Suba</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/06-04-2022/memoria_feria_de_servicios_localidad_teusaquillo.pdf">Memoria Feria de Servicios Localidad Teusaquillo</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/06-04-2022/memoria_feria_de_servicios_localidad_usaquen.pdf">Memoria Feria de Servicios Localidad Usaquén</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/06-04-2022/memoria_feria_de_servicios_localidad_barrios_unidos.pdf">Memoria Feria de Servicios Localidad Barrios Unidos</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/06-04-2022/memoria_feria_de_servicios_localidad_chapinero.pdf">Memoria Feria de Localidad Chapinero</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/06-05-2022/memoria_feria_de_servicios_localidad_puente_aranda.pdf">Memoria Feria de Servicios Localidad Puente Aranda</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/10-05-2022/memoria_feria_de_servicios_localidad_tunjuelito.pdf">Memoria Feria de Servicios Localidad Tunjuelito</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/20-05-2022/memoria_feria_de_servicios_localidad_candelaria_-_santa_fe.pdf">Memoria Feria de Servicios Localidad Candelaria - Santa Fe</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-05-2022/memoria_feria_de_servicios_localidad_san_cristobal_0.pdf">Memoria Feria de Servicios Localidad San Cristóbal</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/13-06-2022/memoria_feria_de_servicios_localidad_rafael_uribe_uribe.pdf">Memoria&nbsp;Feria&nbsp;de&nbsp;Servicios Localidad Rafael Uribe Uribe</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/09-08-2022/memoria_feria_de_servicios_localidad_antonio_narino_0.pdf">Memoria Feria de Servicios Localidad Antonio Nariño</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/19-08-2022/memoria_feria_de_servicios_local_engativa.pdf">Memoria de la Feria de servicios Alcaldía Local Engativá</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/19-08-2022/memoria_feria_de_servicios_semana_distrital_de_la_juventud.pdf">Memoria de la Feria de servicios Semana Distrital de la Juventud</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <!-- /#fin _2022 -->
+
+</div>
+<!-- /#fin  ferias de servicio -->
+
