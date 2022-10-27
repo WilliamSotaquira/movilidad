313a852d6ab012e0774764e66ff4113c60714f36
WilliamSotaquira <william.sotaquira@gmail.com>
2022-10-27 13:33:00 -0500

    Creación de pagina web del Sistema de Bicicletas Compartidas en Bogotá

--- resources/views/2022/09/biciCompartida.blade.php
+++ resources/views/2022/09/biciCompartida.blade.php
@@ -24,6 +24,22 @@
                 </div>
             </div>
         </div>
+        <div class="box-parrafo">
+            <p>¡Es un hecho! Bogotá ya cuenta con su primer Sistema de Bicicletas Compartidas y tú estás invitado a disfrutar de esta nueva opción de movilidad sostenible que hemos dispuesto para ti.</p>
+        </div>
+        <style>
+            .box-wrp .box-parrafo {
+                padding: 32px 32px 0px 32px;
+            }
+
+            .box-wrp .box-parrafo p {
+                font-size: 16px;
+                font-weight: 500;
+                text-align: left;
+                line-height: 1.4;
+                color: rgba(25, 25, 25, 1);
+            }
+        </style>
 
         <box class="video-ppal">
             <div class='video'>
@@ -37,16 +53,16 @@
             <div class="box-botones">
                 <a data-toggle="tab" href="#bici1">
                     <div class="boton zoom">
-                        <h3 class="wow animate__animated animate__bounceInUp animate__delay-1s animate__slow">1500</h3>
+                        <h3 class="wow animate__animated animate__bounceIn animate__delay-1s animate__slow">1500</h3>
                         <!-- <span><img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/08/Icon-Monocleta.svg' alt='' title=''></span> -->
                         <span class="wow animate__animated animate__heartBeat animate__delay-1s animate__slower animate__repeat-2"><i class="fa-solid fa-person-biking fa-2x"></i></span>
                         <h4>Bicicletas Mecánicas</h4>
                     </div>
                 </a>
 
-                <a data-toggle="tab" href="#bici4">
+                <a data-toggle="tab" href="#bici2">
                     <div class="boton zoom">
-                        <h3 class="wow animate__animated animate__bounceInUp animate__delay-1s animate__slow">150</h3>
+                        <h3 class="wow animate__animated animate__bounceIn animate__delay-1s animate__slow">150</h3>
                         <span class="wow animate__animated animate__heartBeat animate__delay-1s animate__slower animate__repeat-2"><img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/08/Icon-Monocleta.svg' alt='' title=''></span>
                         <h4>Manocletas </h4>
                     </div>
@@ -54,16 +70,16 @@
 
                 <a data-toggle="tab" href="#bici3">
                     <div class="boton zoom">
-                        <h3 class="wow animate__animated animate__bounceInUp animate__delay-1s animate__slow">1500</h3>
+                        <h3 class="wow animate__animated animate__bounceIn animate__delay-1s animate__slow">1500</h3>
                         <!-- <span><img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/08/Icon-Monocleta.svg' alt='' title=''></span> -->
                         <span class="wow animate__animated animate__heartBeat animate__delay-1s animate__slower animate__repeat-2"><i class="fa-sharp fa-solid fa-bolt fa-2x"></i></span>
                         <h4>Bicicletas Asistidas</h4>
                     </div>
                 </a>
 
-                <a data-toggle="tab" href="#bici2">
+                <a data-toggle="tab" href="#bici4">
                     <div class="boton zoom">
-                        <h3 class="wow animate__animated animate__bounceInUp animate__delay-1s animate__slow">150</h3>
+                        <h3 class="wow animate__animated animate__bounceIn animate__delay-1s animate__slow">150</h3>
                         <span class="wow animate__animated animate__heartBeat animate__delay-1s animate__slower animate__repeat-2"><img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/08/Icon-Cajon.svg' alt='' title=''></span>
                         <h4>Bicicletas de cajón</h4>
                     </div>
@@ -71,7 +87,7 @@
 
                 <a data-toggle="tab" href="#bici5">
                     <div class="boton zoom">
-                        <h3 class="wow animate__animated animate__bounceInUp animate__delay-1s animate__slow">300</h3>
+                        <h3 class="wow animate__animated animate__bounceIn animate__delay-1s animate__slow">300</h3>
                         <span class="wow animate__animated animate__heartBeat animate__delay-1s animate__slower animate__repeat-2"><i class="fa-solid fa-battery-full fa-2x"></i></span>
 
                         <h4>Estaciones</h4>
@@ -80,13 +96,15 @@
             </div>
 
 
+
             <style>
                 .box-wrp .box-botones {
                     margin-top: 32px;
                     display: grid;
                     gap: 16px;
                     grid-auto-flow: dense;
-                    grid-template-columns: repeat(5, 1fr);
+                    grid-template-columns: repeat(auto-fill, minmax(min(100%, 150px), 1fr));
+                    /* grid-template-columns: repeat(5, 1fr); */
                     grid-template-rows: auto;
                 }
 
@@ -100,16 +118,22 @@
                     flex-direction: column;
                     justify-content: center;
                     align-items: center;
-                    border-radius: 16px;
-                    border: solid 2px rgba(147, 195, 221, 1);
+                    border-radius: 8px;
+                    border: solid 2px rgba(147, 195, 221, 0.5);
+                }
+
+                .box-wrp .box-botones .boton:hover {
+
+                    -webkit-box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.16);
+                    box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.16);
                 }
 
                 .box-wrp .box-botones .boton h3 {
                     font-size: 40px;
-                    font-weight: 600;
+                    font-weight: 400;
                     text-align: center;
                     line-height: 1;
-                    color: rgba(0, 69, 221, 1);
+                    color: #0045dd;
                 }
 
                 .box-wrp .box-botones .boton h4 {
@@ -147,35 +171,113 @@
             <div class="tab-content">
 
                 <div id="bici1" class="tab-pane fade in active">
-                    <h3>Bicicletas Mecánicas</h3>
-                    <p>Some content in menu 1.</p>
-                    <div class='imagen-bici'>
-                        <img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/07/GIF-Bike-fit_sem-numero.gif-bici' alt='' title=''>
+                    <div class="content">
+                        <h3>Bicicletas Mecánicas</h3>
+                        <!-- <p>Some content in menu 1.</p> -->
+                        <div class='imagen-bici'>
+                            <img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/07/GIF-Bike-fit_sem-numero.gif' alt='' title=''>
+                        </div>
                     </div>
                 </div>
                 <div id="bici2" class="tab-pane fade">
-                    <h3>Menu 2</h3>
-                    <p>Some content in menu 2.</p>
+                    <div class="content">
+                        <h3>Manocletas</h3>
+                        <!-- <p>Some content in menu 2.</p> -->
+                        <div class='imagen-bici'>
+                            <img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/08/Monocleta.jpg' alt='' title=''>
+                        </div>
+                    </div>
                 </div>
                 <div id="bici3" class="tab-pane fade">
-                    <h3>Menu 3</h3>
-                    <p>Some content in menu 3.</p>
+                    <div class="content">
+                        <h3>Bicicletas Asistidas</h3>
+                        <!-- <p>Some content in menu 3.</p> -->
+                        <div class='imagen-bici'>
+                            <img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/08/Layout-Bogota-2_eletrica.gif' alt='' title=''>
+                        </div>
+                    </div>
                 </div>
                 <div id="bici4" class="tab-pane fade">
-                    <h3>Menu 4</h3>
-                    <p>Some content in menu 4.</p>
+                    <div class="content">
+                        <h3>Bicicletas de Cajón</h3>
+                        <!-- <p>Some content in menu 4.</p> -->
+                        <div class='imagen-bici'>
+                            <img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/08/Bici-Cajon.jpg' alt='' title=''>
+                        </div>
+                    </div>
                 </div>
                 <div id="bici5" class="tab-pane fade">
-                    <h3>Menu 5</h3>
-                    <p>Some content in menu 5.</p>
+                    <div class="content">
+                        <h3>Estaciones</h3>
+                        <!-- <p>Some content in menu 5.</p> -->
+                        <div class='imagen-bici'>
+                            <img class='img-responsive w-100' src='https://tembici.com.co/wp-content/uploads/2022/07/Gif-Estacao_Sem-Numero.gif' alt='' title=''>
+                        </div>
+                    </div>
                 </div>
             </div>
+
             <style>
-                /* .box-wrp .imagen-bici{
-                    max-width: 80%;
+                .box-wrp .imagen-bici {
+                    max-width: 60%;
                     margin: auto;
-                } */
+                }
+
+                .box-contador .tab-content .tab-pane {}
+
+                .box-contador .tab-content .tab-pane h3 {
+                    text-align: center !important;
+                    font-size: 34px;
+                    font-weight: 500;
+                    line-height: 1.3;
+                    color: #0045dd;
+                    padding: 32px;
+                    border-bottom: solid 2px #f9008310;
+
+                }
             </style>
+        </div>
+        <box class="video-ppal">
+            <h3>Usar el Sistema de Bicicletas Compartidas es muy sencillo</h3>
+            <div class='video'>
+                <div class='embed-responsive embed-responsive-16by9'>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/NpEaa2P7qZI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </box>
+        <style>
+            .box-wrp .video-ppal h3 {
+                background: #f90083;
+                text-align: center !important;
+                font-size: 34px;
+                font-weight: 500;
+                line-height: 1.3;
+                color: #fff;
+                margin: 0px;
+                padding: 32px;
+            }
+
+            .box-wrp .video-ppal {
+                text-align: center !important;
+                font-size: 34px;
+                font-weight: 500;
+                line-height: 1.3;
+                /* border-bottom: solid 2px #f9008310; */
+
+            }
+
+            .box-wrp .video-ppal .video {
+
+                padding: 32px 32px 0px 32px;
+                /* border-bottom: solid 2px #f9008310; */
+
+            }
+        </style>
+        <div class="box-pasoPaso">
+        </div>
+
+        <div class="box-mapa">
+            <iframe src="https://planurb.carto.com/builder/a09ef5b0-ca57-407e-93e2-22f400b18cde/embed#/" width="100%" height="500" frameborder="0" sandbox="allow-forms allow-scripts"></iframe>
         </div>
 
     </div>
