792c19dc9ff4b5a8f4dbaa8cfb0e8841ca9d178d
WilliamSotaquira <william.sotaquira@gmail.com>
2022-10-27 13:53:24 -0500

    Creación de pagina web y consolidación de infomacion de "Agenda Regulatoria"

---
+++ resources/views/2022/10/regulatoria.blade.php
@@ -0,0 +1,91 @@
+<hr>
+<p>En esta sección encontrará la lista de los proyectos específicos de regulación que previsiblemente deban expedirse por la entidad durante el año siguiente y sus actualizaciones</p>
+<br>
+
+<!-- collapse_agendaRegulatoria -->
+<div class='panel-group' id='accordion_agendaRegulatoria' role='tablist ' aria-multiselectable='true'>
+
+    <!-- collapse_proyectos -->
+    <div class='panel panel-primary'>
+        <div class='panel-heading' role='tab' id='heading_proyectos'>
+            <h4 class='panel-title'>
+                <a role='button' data-toggle='collapse' data-parent='#accordion_agendaRegulatoria' href='#collapse_proyectos' aria-expanded='true' aria-controls='collapse_proyectos'>
+                    Proyectos Agenda Regulatoria Publicados para Observaciones
+                </a>
+            </h4>
+        </div>
+        <div id='collapse_proyectos' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_proyectos'>
+            <div class='panel-body'>
+                <!-- <blockquote>Se publica el Proyecto de Agenda Regulatoria para el año 2022 de la Secretaría Distrital de Movilidad para observaciones de la ciudadanía y/o gurpos de interés hasta el día 20 de diciembre de 2021 al correo normatividad@movilidadbogota.gov.co</blockquote> -->
+
+                <ul>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/07-10-2022/proyecto_agenda_regulatoria_2022.xlsx" target="_blank" rel="noopener noreferrer">Proyecto Agenda Regulatoria 2022</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /#fin _proyectos -->
+
+    <!-- collapse_agenda -->
+    <div class='panel panel-primary'>
+        <div class='panel-heading' role='tab' id='heading_agenda'>
+            <h4 class='panel-title'>
+                <a role='button' data-toggle='collapse' data-parent='#accordion_agendaRegulatoria' href='#collapse_agenda' aria-expanded='true' aria-controls='collapse_agenda'>
+                    Agenda Regulatoria y Actualizaciones
+                </a>
+            </h4>
+        </div>
+        <div id='collapse_agenda' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_agenda'>
+            <div class='panel-body'>
+
+                <!-- collapse_historicoAgendas -->
+                <div class='panel-group' id='accordion_historicoAgendas' role='tablist ' aria-multiselectable='true'>
+
+                    <!-- collapse_agendas2022 -->
+                    <div class='panel panel-primary'>
+                        <div class='panel-heading' role='tab' id='heading_agendas2022'>
+                            <h4 class='panel-title'>
+                                <a role='button' data-toggle='collapse' data-parent='#accordion_historicoAgendas' href='#collapse_agendas2022' aria-expanded='true' aria-controls='collapse_agendas2022'>
+                                    2022
+                                </a>
+                            </h4>
+                        </div>
+                        <div id='collapse_agendas2022' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_agendas2022'>
+                            <div class='panel-body'>
+
+                                <ul>
+                                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/11-10-2022/agenda_regulatoria_actualizada_sdm_06_10_2022.xlsx" target="_blank" rel="noopener noreferrer">Agenda Regulatoria - octubre 2022</a></li>
+                                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/12-09-2022/agenda_regulatoria_actualizada_sdm_06_09_2022_vf.xlsx" target="_blank" rel="noopener noreferrer">Agenda Regulatoria - septiembre 2022</a></li>
+                                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/03-08-2022/agenda_regulatoria_actualizada_sdm_02_08_2022.xlsx" target="_blank" rel="noopener noreferrer">Agenda Regulatoria - agosto 2022</a></li>
+                                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/08-07-2022/agenda_regulatoria_actualizada_sdm_07_07_2022_0.xlsx" target="_blank" rel="noopener noreferrer">Agenda Regulatoria - julio 2022</a></li>
+                                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/07-06-2022/agenda_regulatoria_actualizada_sdm_03_06_2022.xlsx" target="_blank" rel="noopener noreferrer">Agenda Regulatoria - junio 2022</a></li>
+                                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/16-05-2022/agenda_regulatoria_actualizada_sdm_12_05_2022.xlsx" target="_blank" rel="noopener noreferrer">Agenda Regulatoria - mayo 2022</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /#fin _agendas2022 -->
+
+
+                </div>
+                <!-- /#fin _historicoAgendas -->
+
+
+
+            </div>
+        </div>
+    </div>
+    <!-- /#fin _agenda -->
+
+</div>
+<!-- /#fin _agendaRegulatoria -->
+
+
+
+<!-- Versiones  -->
+<!-- Se consolida la información de las Agendas Regulatorias y Proyecto de Agenda Regulatoria en una sola sección https://www.movilidadbogota.gov.co/web/proyecto_de_agenda_regulatoria_2022 -->
+<!-- Fin Versiones  -->
+
+<style>
+
+</style>
