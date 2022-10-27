88ef1ed30b2c6dd4249e86fd42f619da1f737918
WilliamSotaquira <william.sotaquira@gmail.com>
2022-10-27 13:50:47 -0500

    Creación de la pagina web "Registro de Publicaciones" para la sección de Transparencia y en cumplimiento a la Resolución 1519 de 2020

---
+++ resources/views/2022/10/pubicaciones.blade.php
@@ -0,0 +1,48 @@
+<hr>
+<p>Listado de documentos publicados en el sitio web relacionados con el cumplimiento de la Ley 1712 de 2014 y el Decreto Reglamentario 103 de 2015.</p>
+<br>
+<!-- collapse_publicaciones -->
+<div class='panel panel-primary'>
+    <div class='panel-heading' role='tab' id='heading_publicaciones'>
+        <h4 class='panel-title'>
+            <a role='button' data-toggle='collapse' data-parent='#accordion' href='#collapse_publicaciones' aria-expanded='true' aria-controls='collapse_publicaciones'>
+                Registro de Publicaciones SDM
+            </a>
+        </h4>
+    </div>
+    <div id='collapse_publicaciones' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_publicaciones'>
+        <div class='panel-body'>
+
+        <!-- collapse_registroPublicaciones -->
+<div class='panel-group' id='accordion_registroPublicaciones' role='tablist ' aria-multiselectable='true'>
+
+<!-- collapse_2022 -->
+<div class='panel panel-primary'>
+    <div class='panel-heading' role='tab' id='heading_2022'>
+        <h4 class='panel-title'>
+            <a role='button' data-toggle='collapse' data-parent='#accordion' href='#collapse_2022' aria-expanded='true' aria-controls='collapse_2022'>
+                2022
+            </a>
+        </h4>
+    </div>
+    <div id='collapse_2022' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_2022'>
+        <div class='panel-body'>
+            <ul>
+                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/01-10-2022/registro_de_publiaciones_-_septiembre_2022.xlsx" target="_blank" rel="noopener noreferrer" download>Septiembre</a></li>
+                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/01-10-2022/registro_de_publiaciones_-_agosto_2022.xlsx" target="_blank" rel="noopener noreferrer" download>Agosto</a></li>
+                <li><a href="www.movilidadbogota.gov.co/web/sites/default/files/Paginas/22-08-2022/remision_de_informacion_para_pub.xls" target="_blank" rel="noopener noreferrer" download>Julio</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /#fin _2022 -->
+
+
+</div>
+<!-- /#fin _registroPublicaciones -->
+
+
+        </div>
+    </div>
+</div>
+<!-- /#fin _publicaciones -->
