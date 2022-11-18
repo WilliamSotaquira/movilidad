abc226092c8765fb34acbd118de1ae6777c6813c
WilliamSotaquira <william.sotaquira@gmail.com>
2022-11-18 16:01:59 -0500

    Subida y bajada de los diferentes avisos de mantenimiento a los sitios web de la Entidad

--- resources/views/inicio/avisos.blade.php
+++ resources/views/inicio/avisos.blade.php
@@ -1,60 +1,39 @@
 @extends('welcome')
 @section('contenido')
 
-<!-- Sección Bloque de Mantenimiento  -->
-<div class="box-avisos">
-
-    <hr>
-    <!-- <div class="row"> -->
-    <!-- banner-xs  -->
-    <!-- <div class="col-xs-12 visible-xs-12 visible-xs-block">
-            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/14-10-2022/suspension_de_servicios_v2_-_benner_900x300.jpg" title="Aviso Mantenimiento Sistema Contravencional">
-        </div> -->
-    <!-- Fin banner-xs  -->
-
-    <!-- banner-lg  -->
-    <!-- <div class="col-sm-12 hidden-xs">
-            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/14-10-2022/suspension_de_servicios_v2_-_banner_1920x640.jpg" title="Aviso Mantenimiento Sistema Contravencional">
-        </div> -->
-    <!-- Fin banner-lg  -->
-    <!-- </div> -->
-    <!-- <hr> -->
-
-</div>
-<!-- Fin Sección Bloque de Mantenimiento  -->
-
 <!-- Styles -->
 <style type='text/css'>
-    .field-item.even h2 {
-        visibility: hidden;
-        margin-top: -50px;
-    }
 
     .region-content {
         padding: 0px;
-        overflow: hidden;
     }
 
     .box-avisos {
         max-width: 980px;
         word-wrap: break-word;
         margin: auto;
-
     }
-
-    /*
-    . [class*='col-'] {
-        margin: 0px;
-        padding: 0px;
-        border: 0;
-        outline: 0;
-        font-size: 100%;
-        vertical-align: baseline;
-    }
-    */
 
     .w-100 {
         width: 100%;
     }
 </style>
 <!-- End Styles -->
+
+
+<!-- Sección Bloque de Mantenimiento  -->
+<!-- Primero banner-xs, segundo banner-lg  -->
+<div class="box-avisos">
+    <!-- <hr>
+    <div class="row">
+        <div class="col-xs-12 visible-xs-12 visible-xs-block">
+            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/11-11-2022/2022-11-11_favor-aviso-suspension-de-servicios_900x300.png" title="Aviso Mantenimiento Sistema Contravencional">
+        </div>
+        <div class="col-sm-12 hidden-xs">
+            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/11-11-2022/2022-11-11_favor-aviso-suspension-de-servicios_1920x640.png" title="Aviso Mantenimiento Sistema Contravencional">
+        </div>
+    </div> -->
+    <hr>
+</div>
+<!-- Fin Sección Bloque de Mantenimiento  -->
+
