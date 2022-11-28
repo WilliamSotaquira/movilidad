d176a0b4ea231ceca516f8a914be4e89f69c0a96
WilliamSotaquira <william.sotaquira@gmail.com>
2022-11-28 10:34:29 -0500

    Actualización de la pagina web Organigrama de la SDM

--- resources/views/especifico/entidad/organigrama.blade.php
+++ resources/views/especifico/entidad/organigrama.blade.php
@@ -13,7 +13,7 @@
     <div class="row">
         <div class="col-xs-12 text-center">
             <div class="boton_org">
-                <a class="btn-org" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/15-11-2022/organigrama9nov2022.pdf" target="_blank" rel="noopener noreferrer">Ver Organigrama Completo</a>
+                <a class="btn-org" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/23-11-2022/organigramanoviembre20221123.pdf" target="_blank" rel="noopener noreferrer">Ver Organigrama Completo</a>
             </div>
         </div>
     </div>
@@ -2085,7 +2085,7 @@
                                                 <p>Formula y dirige la formulación, ejecución y evaluación de las políticas, programas, planes y proyectos referentes al talento humano en la entidad, buscando el desarrollo integral del personal y cumpliendo con las disposiciones legales vigentes y objetivos institucionales de la Secretaría Distrital Movilidad.</p>
                                             </div>
                                             <div class='panel-body-mapa'>
-
+<!--
                                                 <div class="row">
                                                     <div class="col-xs-12 col-sm-4 text-center">
                                                         <div class="img-perfil w-100">
@@ -2101,6 +2101,26 @@
                                                                 <p>Gracias a sus 20 años de experiencia y formación conoce la normativa y elementos básicos de la legislación laboral; el diseño, elaboración, ejecución y celebración de programas, proyectos y contratos en la administración pública.</p>
                                                                 <p>Trabajó en la Contraloría de Bogotá donde desempeñó cargos como: Subdirectora Bienestar Social, Subdirectora de Jurisdicción Coactiva de la Dirección de Responsabilidad Fiscal y Jurisdicción Coactiva, y Subdirectora de Infraestructura de la Dirección de Movilidad. Además, estuvo vinculada a entidades como: Personería de Bogotá, Fiscalía General de la Nación, Procuraduría General de la Nación y Tribunal Superior de Bogotá.</p>
                                                                 <p>Asume este cargo con el objetivo de aportar desde su conocimiento al bienestar del talento humano de la Secretaría, pues está convencida de que el sentido de pertenencia de los colaboradores de la entidad es clave para lograr todos los objetivos propuestos.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div> -->
+
+                                                <div class="row">
+                                                    <div class="col-xs-12 col-sm-4 text-center">
+                                                        <div class="img-perfil w-100">
+                                                            <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/28-07-2022/paola_adriana_corona_miranda.png" alt="Paola Adriana Corona Miranda" title="Paola Adriana Corona Miranda">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xs-12 col-sm-8 ">
+                                                        <div class="descripcion">
+                                                            <h1>Paola Adriana Corona Miranda</h1>
+                                                            <h4>Directora de Talento Humano (e)</h4>
+                                                            <div class="descripcion-xs">
+                                                                <p>Es administradora de empresas y con especialización en Finanzas y Administración Pública de la Universidad Militar Nueva Granada. Es líder de la política de derechos humanos y empresa del sector de comercio, industria y turismo. Líder en temas de responsabilidad social, derechos humanos y manager en la gestión de la conciliación de EFR (Empresa Familiarmente Responsable), de la fundación MAS Familia de España.</p>
+                                                                <p>También está certificada como Auditoria de Calidad Seguridad y Salud en el Trabajo y el Medio Ambiente y tiene cursos en gestión de proyectos.</p>
+                                                                <p>Por espacio de 10 años estuvo al servicio de la Superintendencia de Industria y Comercio, donde se desempeñó en la Secretaría General, apoyando todos los procesos de talento humano, gestión documental, financiera y recursos físicos, entre otros.</p>
+
                                                             </div>
                                                         </div>
                                                     </div>
