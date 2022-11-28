40f4767ec38a74592ece9a3fd9115fc2d1711530
WilliamSotaquira <william.sotaquira@gmail.com>
2022-11-28 10:36:11 -0500

    Carga de los avisos de mantenimiento de las tecnologías de los sitios web de la SDM

--- resources/views/inicio/avisos.blade.php
+++ resources/views/inicio/avisos.blade.php
@@ -27,10 +27,10 @@
     <!-- <hr>
     <div class="row">
         <div class="col-xs-12 visible-xs-12 visible-xs-block">
-            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/11-11-2022/2022-11-11_favor-aviso-suspension-de-servicios_900x300.png" title="Aviso Mantenimiento Sistema Contravencional">
+            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/25-11-2022/aviso_de_suspencion_de_servicios_benner_900x300.jpg" title="Aviso Mantenimiento Sistema Contravencional">
         </div>
         <div class="col-sm-12 hidden-xs">
-            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/11-11-2022/2022-11-11_favor-aviso-suspension-de-servicios_1920x640.png" title="Aviso Mantenimiento Sistema Contravencional">
+            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/25-11-2022/aviso_de_suspencion_de_servicios_banner_1920x640.jpg" title="Aviso Mantenimiento Sistema Contravencional">
         </div>
     </div> -->
     <hr>
