528884aaa8951a4403e0b7148c77223c826a9a5a
WilliamSotaquira <william.sotaquira@gmail.com>
2023-01-20 16:49:48 -0500

    Actualización permanente a las diferntes ventanas de mantenimiento de la Entidad

--- resources/views/inicio/avisos.blade.php
+++ resources/views/inicio/avisos.blade.php
@@ -3,6 +3,15 @@
 
 <!-- Styles -->
 <style type='text/css'>
+    :root {
+        --color-primario: #4c531e;
+        --color-secundario: #879225;
+        --color-terciario: #bed000;
+        --color-gris: #e5e5e5;
+        --color-blanco: #ffffff;
+        --color-negro: #191919;
+        --color-fondo: #333333;
+    }
 
     .region-content {
         padding: 0px;
@@ -12,28 +21,34 @@
         max-width: 980px;
         word-wrap: break-word;
         margin: auto;
+
     }
 
     .w-100 {
         width: 100%;
     }
+
+
 </style>
 <!-- End Styles -->
 
 
+
+
 <!-- Sección Bloque de Mantenimiento  -->
 <!-- Primero banner-xs, segundo banner-lg  -->
+
 <div class="box-avisos">
-    <!-- <hr>
+    <hr>
     <div class="row">
         <div class="col-xs-12 visible-xs-12 visible-xs-block">
-            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/25-11-2022/aviso_de_suspencion_de_servicios_benner_900x300.jpg" title="Aviso Mantenimiento Sistema Contravencional">
+            <img alt="Aviso Mantenimiento" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/22-12-2022/banner_600x300_copia-100.jpg" title="Aviso Mantenimiento">
         </div>
         <div class="col-sm-12 hidden-xs">
-            <img alt="Aviso Mantenimiento Sistema Contravencional" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/25-11-2022/aviso_de_suspencion_de_servicios_banner_1920x640.jpg" title="Aviso Mantenimiento Sistema Contravencional">
+            <img alt="Aviso Mantenimiento" class="img-responsive bs zoom w-100 img-rounded" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/22-12-2022/banner_1920x640_copia-100.jpg" title="Aviso Mantenimiento">
         </div>
-    </div> -->
+    </div>
     <hr>
 </div>
-<!-- Fin Sección Bloque de Mantenimiento  -->
 
+<!-- Fin Sección Bloque de Mantenimiento  -->
