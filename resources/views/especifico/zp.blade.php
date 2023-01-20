63691df564e28911a301dbd10d1e87cd400f3f02
WilliamSotaquira <william.sotaquira@gmail.com>
2023-01-20 16:44:53 -0500

    Pagina entorno anónimo de pruebas ZP

---
+++ resources/views/especifico/zp.blade.php
@@ -0,0 +1,47 @@
+<hr>
+<p>Este esquema presenta toda la información alojada en la página para conocimiento y acceso público, describiendo entre otras características el formato del documento, la fecha de generación del mismo, su frecuencia de actualización, y el área o dependencia responsable de su producción y actualización. La Secretaría Distrital de Movilidad hace consolidación de esta información mensualmente.</p>
+
+<p><br>
+    <!-- collapse_esquema -->
+</p>
+
+<div aria-multiselectable="true" class="panel-group" id="accordion_esquema" role="tablist ">
+
+    <!-- collapse_transparencia -->
+    <div class="panel panel-primary">
+        <div class="panel-heading" id="heading_transparencia" role="tab">
+            <h4 class="panel-title"><a aria-controls="collapse_transparencia" aria-expanded="true" data-parent="#accordion_esquema" data-toggle="collapse" href="#collapse_transparencia" role="button">Esquema de Publicación de Información </a></h4>
+        </div>
+
+        <div aria-labelledby="heading_transparencia" class="panel-collapse collapse in" id="collapse_transparencia" role="tabpanel">
+            <div class="panel-body">
+
+                <ul>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/30-12-2022/esquema_de_publicacion_-_octubre_2022_1.xlsx" rel="noopener noreferrer" target="_blank">Esquema de publicación - octubre (Excel descargable)</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <!-- /#fin _transparencia -->
+
+    <!-- collapse-publicacionesMinimas -->
+    <div class="panel panel-primary">
+        <div class="panel-heading" id="heading-publicacionesMinimas" role="tab">
+            <h4 class="panel-title"><a aria-controls="collapse-publicacionesMinimas" aria-expanded="true" data-parent="#accordion_esquema" data-toggle="collapse" href="#collapse-publicacionesMinimas" role="button">Matriz de Cumplimiento de las Publicaciones Mínimas </a></h4>
+        </div>
+
+
+        <div aria-labelledby="heading-publicacionesMinimas" class="panel-collapse collapse in" id="collapse-publicacionesMinimas" role="tabpanel">
+            <div class="panel-body">
+                <ul>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/12-12-2022/matriz_de_cumplimiento_de_las_publicaciones_minimas_-_noviembre_2022_0.xlsx" rel="noopener noreferrer" target="_blank">Matriz de Cumplimiento de las Publicaciones Mínimas - noviembre (Excel descargable)</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <!-- /#fin -publicacionesMinimas -->
+
+</div>
+<!-- /#fin _esquema -->
