813788cd04637e29cb1b2268eb2bf968092528fc
WilliamSotaquira <william.sotaquira@gmail.com>
2023-01-24 17:01:30 -0500

    Creación de las paginas necesarias para el mantenimiento del "Header"

---
+++ resources/views/inicio/accesibilidad.blade.php
@@ -0,0 +1,36 @@
+<div class="accesibilidad-bar justify-center">
+    <div class="row">
+        <div class="col-xs-3 col-sm-4 col-md-4 col-lg-4"><a href="https://www.gov.co/home/"><img class="img-responsive w-100" alt="gov.co" src="http://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/14-05-2021/geadergov.png"></a></div>
+
+        <div class="hidden-xs col-xs-3 col-sm-4 col-md-4 col-lg-4">
+
+            <div id="google_translate_element"></div>
+            <script type="text/javascript">
+                function googleTranslateElementInit() {
+                    new google.translate.TranslateElement({
+                        pageLanguage: 'es',
+                        includedLanguages: 'ca,de,en,es,fr,it,ja,zh-TW',
+                        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                        gaTrack: true,
+                        gaId: 'UA-65129685-1'
+                    }, 'google_translate_element');
+                }
+            </script>
+            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        </div>
+
+        <div class="col-xs-6 hidden-xs col-sm-4 col-md-4 col-lg-4">
+            <h5><a href="http://www.movilidadbogota.gov.co/web/accesibilidad-web">Ir a instrucciones de accesibilidad web</a></h5>
+        </div>
+    </div>
+</div>
+
+
+
+
+<style>
+    .accesibilidad-bar {
+        background-color: #454545;
+        text-align: center;
+    }
+</style>
---
+++ resources/views/inicio/logos-header.blade.php
@@ -0,0 +1,74 @@
+<html style="height:100%" lang="es">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+<script src="https://www.googleoptimize.com/optimize.js?id=GTM-KTCJW66"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-65129685-1"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&key=AIzaSyCZeMGFx7jmnT_zgndp3YEkzBqXUoCHWy4"></script>
+
+
+
+
+
+<script>
+    $(document).ready(function() {
+        $("html").attr("lang", "gl-ES")
+    });
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-65129685-1', {
+        'optimize_id': 'GTM-KTCJW66'
+    });
+</script>
+
+
+<style>
+    .sticky-enabled {
+        width: 100%;
+    }
+
+    tr.even,
+    tr.odd {
+        background-color: #FFF !important;
+    }
+
+    ul.inline,
+    ul.links.inline {
+        display: none;
+    }
+
+    .metadata-user .username {
+        display: none;
+    }
+
+    .centertil {
+        text-align: center;
+
+    }
+</style>
+<script>
+    (function(d) {
+        var s = d.createElement("script");
+        s.setAttribute("data-account", "xZX3HwT4ev");
+        s.setAttribute("src", "https://cdn.userway.org/widget.js");
+        (d.body || d.head).appendChild(s);
+    })(document)
+</script><noscript>Please ensure Javascript is enabled for purposes of <a href="https://userway.org">website accessibility</a></noscript>
+<link href="https://www.movilidadbogota.gov.co/web/sites/all/libraries/bootstrap/bootstrap.min.css" rel="stylesheet" />
+<link href="https://www.movilidadbogota.gov.co/web/sites/all/libraries/bootstrap/micss.css" rel="stylesheet" />
+<script src="https://www.movilidadbogota.gov.co/web/sites/all/libraries/bootstrap/jquery.min.js"></script>
+<script src="https://www.movilidadbogota.gov.co/web/sites/all/libraries/bootstrap/bootstrap.min.js"></script>
+<html style="height:100%">
+<div class="row">
+    <div class="col-xs-8 col-sm-8 col-md-10 col-lg-10"><a href="https://www.movilidadbogota.gov.co/web/" id="logo" rel="home" title="Secretaría Distrital de Movilidad"><img alt="Secretaría Distrital de Movilidad" class="img-responsive" src="https://www.movilidadbogota.gov.co/web/sites/default/files/logo-sdm.png">
+        </a></div>
+    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2>
+		<div class=" escudo"><img class="img-responsive" src="https://www.movilidadbogota.gov.co/web/sites/all/themes/sdmtheme/images/escudo.png" alt="Escudo de la República de Colombia" title="Escudo de la República de Colombia"></div>
+</div>
+
+
+
