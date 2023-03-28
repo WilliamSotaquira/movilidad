c14f3942fee2b44995230c058c9fe9a075248031
WilliamSotaquira <william.sotaquira@gmail.com>
2023-03-28 15:28:10 -0500

    Adición en la pagina web "Nuestra Entidad" del botón de tipo redirección a la plataforma administrativa KACTUS

--- resources/views/2021/diciembre/nuestraEntidad.blade.php
+++ resources/views/2021/diciembre/nuestraEntidad.blade.php
@@ -1,46 +1,117 @@
-<style type="text/css">
-    .field-item h2 {
-        font-size: 30px;
-        color: #bed000;
-        text-align: center;
-        font-weight: 600;
-        margin-bottom: 30px;
-    }
+@extends('welcome')
+@section('title','nuestraEntidad')
 
-    .img-center {
-        display: block;
-        margin: 0 auto;
-    }
+@section('contenido')
 
-    .btn-servicio {
-        max-width: 70%;
-        transition: ease all .5s;
-    }
+<!-- Set nuestraEntidad -->
+<div class="set-wrapper nuestraEntidad">
 
-    .btn-servicio:hover {
-        max-width: 80%;
-    }
-
-    .texto-intro {
-        font-size: 18px;
-        margin: 10px 10px 40px 10px;
-        text-align: center;
-    }
-
-    .seccion .col-sm-2 {
-        padding-left: 0px;
-        padding-right: 0px;
-    }
-</style>
-<div class="row seccion">
-    <div class="col-sm-12">
-        <p class="texto-intro">Aquí podrás acceder a los principales aplicativos de la SDM, correo institucional e Intranet, a un solo clic toda la información que necesitas para lograr tus actividades diarias.</p>
+    <!-- Component box title-center -->
+    <div class="box-title-center">
+        <h2>Nuestra Entidad</h2>
+        <div>
+            <p class="texto-intro">Aquí podrás acceder a los principales aplicativos de la SDM, correo institucional e Intranet, a un solo clic toda la información que necesitas para lograr tus actividades diarias.</p>
+        </div>
     </div>
-    <div class="col-sm-2 col-xs-6"><a href="https://gestiondocumental.movilidadbogota.gov.co/" target="_blank"><img alt="orfeo" class="img-responsive img-center btn-servicio" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-11-2020/orfeo.png"></a></div>
-    <div class="col-sm-2 col-xs-6"><a href="https://intranetmovilidad.movilidadbogota.gov.co/intranet/" target="_blank"><img alt="" class="img-responsive img-center btn-servicio" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-11-2020/intranet-claro.png"></a></div>
-    <div class="col-sm-2 col-xs-6"><a href="http://mioficina.movilidadbogota.gov.co/" target="_blank"><img alt="" class="img-responsive img-center btn-servicio" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-11-2020/correo-oscuro.png"></a></div>
-    <div class="col-sm-2 col-xs-6"><a href="http://aranda.movilidadbogota.gov.co/" target="_blank"><img alt="" class="img-responsive img-center btn-servicio" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-11-2020/aranda-claro.png"></a></div>
-    <div class="col-sm-2 col-xs-6"><a href="https://www.movilidadbogota.gov.co/web/trabajo_inteligente" target="_blank"><img alt="trabajo inteligente" class="img-responsive img-center btn-servicio" src="https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/30-09-2021/2021-09-23_botones-pagina-trabajo-inteligente_300-x-300_1_1.png"></a></div>
-    <div class="col-sm-2 col-xs-6"><a href="https://cursos.movilidadbogota.gov.co/" target="_blank"><img alt="acceso a cursos virtuales" class="img-responsive img-center btn-servicio" src="https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/15-12-2021/plataformamoodle.png"></a></div>
+    <style>
+        .field-items .field-item>h2 {
+            visibility: hidden;
+            margin-top: -50px;
+
+        }
+
+        .set-wrapper .box-title-center {
+            grid-area: box-title-center;
+        }
+
+        .box-title-center h2 {
+            text-align: center;
+            font-size: 32px;
+            font-weight: 700;
+            color: rgba(77, 84, 31, 1);
+        }
+    </style>
+
+    <!-- Sección box grid-buttons -->
+    <div class="box-grid-buttons">
+
+        <a href="https://gestiondocumental.movilidadbogota.gov.co/" target="_blank"><img alt="icono del aplicativo ORFEO" class="img-responsive w-100 zoom" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-11-2020/orfeo.png"></a>
+        <a href="https://www.movilidadbogota.gov.co/intranet/" target="_blank"><img alt="icono de la intranet de la SDM" class="img-responsive w-100 zoom" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-11-2020/intranet-claro.png"></a>
+        <a href="http://mioficina.movilidadbogota.gov.co/" target="_blank"><img alt="icono de correo" class="img-responsive w-100 zoom" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-11-2020/correo-oscuro.png"></a>
+        <a href="https://aranda.movilidadbogota.gov.co/USDKV8/Login.aspx" target="_blank"><img alt="icono de ARANDA" class="img-responsive w-100 zoom" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-11-2020/aranda-claro.png"></a>
+        <a href="https://www.movilidadbogota.gov.co/web/trabajo_inteligente" target="_blank"><img alt="trabajo inteligente" class="img-responsive w-100 zoom" src="https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/30-09-2021/2021-09-23_botones-pagina-trabajo-inteligente_300-x-300_1_1.png"></a>
+        <a href="https://cursos.movilidadbogota.gov.co/" target="_blank"><img alt="acceso a cursos virtuales" class="img-responsive w-100 zoom" src="https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/15-12-2021/plataformamoodle.png"></a>
+        <a href="https://simur.gov.co/biblioteca-digital/" target="_blank"><img alt="Biblioteca Digital" class="img-responsive w-100 zoom" src="https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/10-05-2022/iconos-02.jpg"></a>
+        <a href="http://kactus.movilidadbogota.gov.co/SmartPeople" target="_blank"><img alt="Biblioteca Digital" class="img-responsive w-100 zoom" src="https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/24-03-2023/9._anexo_1._aplicativo_de_pago_icono_circular_1_1.png"></a>
+
+
+    </div>
+
+
+    <br>
+    <style>
+        .set-wrapper .box-grid-buttons {
+            grid-area: box-grid-buttons;
+            padding: 2em;
+            display: grid;
+            gap: 32px;
+            grid-auto-flow: dense;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: auto;
+            justify-content: center;
+        }
+        .w-100{
+            min-width: 100%;
+        }
+        .zoom {
+            transition: 0.5s ease;
+            -moz-transition: 0.5s ease;
+            /* Firefox */
+            -webkit-transition: 0.5s ease;
+            /* Chrome - Safari */
+            -o-transition: 0.5s ease;
+            /* Opera */
+            -ms-transition: 0.5s ease;
+            /* IE9 */
+        }
+
+        .zoom:hover {
+            transform: scale(1.05);
+            -moz-transform: scale(1.05);
+            /* Firefox */
+            -webkit-transform: scale(1.05);
+            /* Chrome - Safari */
+            -o-transform: scale(1.05);
+            /* Opera */
+            -ms-transform: scale(1.05);
+            /* IE9 */
+        }
+        .img-center {
+            display: block;
+            margin: 0 auto;
+        }
+
+        .texto-intro {
+            font-size: 18px;
+            margin: 10px 10px 40px 10px;
+            text-align: center;
+        }
+
+        @media(min-width:768px) {
+            .set-wrapper .box-grid-buttons {
+                row-gap: 2em;
+                column-gap: 4em;
+                grid-template-columns: repeat(4, 1fr);
+                padding: 2em 8em;
+        }
+    </style>
+
+
+    <br>
+
+
+
+
+
 </div>
-<hr>
+<!-- Fin Set -->
