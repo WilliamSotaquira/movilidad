1c2307ec55f085fbcdefac55a03238eac4247879
WilliamSotaquira <william.sotaquira@gmail.com>
2023-03-28 15:42:52 -0500

    Ajuste de las reglas de estilo de la pagina web "PQRSD" de la Entidad.  Corrección de enlace del reglamento interno de gestión PQRSD

--- resources/views/2021/septiembre/pqrsd.blade.php
+++ resources/views/2021/septiembre/pqrsd.blade.php
@@ -6,7 +6,359 @@
 <link href="https://fonts.googleapis.com/css2?family=Signika:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
+<style>
+    .field-item.even h2 {
+        visibility: hidden;
+        margin-top: -50px;
+    }
 
+    .region-content {
+        margin: 0;
+        padding: 15px;
+    }
+
+    /* .col-sm-12 {
+        margin: 0 !important;
+        padding: 0 !important;
+    } */
+
+    .seccion {
+        background-color: #fff;
+        max-width: 940px;
+    }
+
+    .w-100 {
+        max-width: 100%;
+    }
+
+    .w-75 {
+        width: 75%;
+    }
+
+    /* .d-flex {
+        display: flex;
+    } */
+
+    .align-items-center {
+        align-items: center;
+    }
+
+    .justify-content-center {
+        justify-content: center;
+    }
+
+    .rh {
+        margin-top: 15px !important;
+    }
+
+    .h3-xs {
+        font-family: 'Signika', sans-serif;
+        margin: 0px !important;
+        font-size: 5rem;
+        background-image: url('https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/28-08-2021/banner_xs_0.png');
+        background-size: 100%;
+        background-repeat: no-repeat;
+    }
+
+    .h3-xs h1 {
+        padding: 6%;
+        margin: 0px 20px 15px 20px;
+        color: #fff;
+        font-weight: 700;
+        font-size: 5rem;
+
+    }
+
+    .h3-sm {
+        font-family: 'Signika', sans-serif;
+        margin: 0px !important;
+        font-size: 5rem;
+        background-image: url('https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/28-08-2021/banner_lg_0.png');
+        background-size: 100%;
+        background-repeat: no-repeat;
+    }
+
+    .h3-sm h1 {
+        padding: 6%;
+        margin: 0px 20px 15px 20px;
+        color: #fff;
+        font-weight: 700;
+        font-size: 40px;
+
+    }
+
+
+    img {
+        /* margin: 5px; */
+        /* width: 0%; */
+
+    }
+
+    .bs {
+        -webkit-box-shadow: 3px 3px 20px -10px rgba(0, 0, 0, 0.78) !important;
+        box-shadow: 3px 3px 20px -10px rgba(0, 0, 0, 0.78) !important;
+    }
+
+    .panel-body {
+        padding: 10px !important;
+        text-align: center;
+        font-size: 2rem;
+        font-weight: 500;
+        color: #080808 !important;
+        /* text-align: justify; */
+        height: 282px;
+        display: flex;
+        line-height: 1.3 !important;
+    }
+
+    .panel-heading {
+        height: 190px;
+
+    }
+
+    .panel-footer {
+        background-color: #fff !important;
+        height: 160px;
+        display: flex;
+        justify-content: center;
+        border-top: none !important;
+        padding: 0px !important;
+
+    }
+
+    .ph-c1 {
+        text-align: center;
+        background-color: #A12B46 !important;
+        color: #fff !important;
+    }
+
+    .ph-c1 h1 {
+        font-size: 6rem !important;
+        font-weight: 700;
+    }
+
+    .ph-c1 h3 {
+        font-size: 2rem !important;
+        font-weight: 600;
+    }
+
+    .ph-c2 {
+        text-align: center;
+        background-color: #8C90F2 !important;
+        color: #080808 !important;
+    }
+
+    .ph-c2 h1 {
+        font-size: 6rem !important;
+        font-weight: 700;
+    }
+
+    .ph-c2 h3 {
+        font-size: 2rem !important;
+        font-weight: 600;
+    }
+
+    .ph-c3 {
+        text-align: center;
+        background-color: #76cbd0ff !important;
+        color: #080808 !important;
+    }
+
+    .ph-c3 h1 {
+        font-size: 6rem !important;
+        font-weight: 700;
+    }
+
+    .ph-c3 h3 {
+        font-size: 2rem !important;
+        font-weight: 600;
+    }
+
+    .c-verde {
+        background-color: #F0F2D9;
+
+    }
+
+    .c-verde h3 {
+        font-weight: 700;
+    }
+
+    .pc1 {
+        background-color: #A12B46;
+        height: 30px;
+        width: 60px;
+    }
+
+    .pc2 {
+        background-color: #8C90F2;
+        height: 30px;
+        width: 60px;
+    }
+
+    .pc3 {
+        background-color: #76cbd0ff;
+        height: 30px;
+        width: 60px;
+    }
+
+    .c-verde-2 {
+        background-color: #c9d41eff;
+        padding: 15px;
+        color: #080808;
+        margin-left: 15px;
+        margin-right: 15px;
+        margin-top: 25px;
+        text-align: center;
+        display: flex;
+        vertical-align: middle;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .c-verde-2 h3 {
+        font-weight: 700;
+    }
+
+    .btn-icon {
+        height: 280px;
+        padding: 0px 25px 25px 25px;
+        margin: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        text-align: center;
+        vertical-align: middle;
+        background-color: #4D541F;
+        color: #fff;
+        border-radius: 4px;
+        /* transition: 0.5s all; */
+    }
+
+    .btn-icon a {
+        color: #fff;
+        text-decoration: none !important;
+    }
+
+    .btn-icon:hover a {
+        color: #000;
+    }
+
+    .btn-icon:hover,
+    .btn-icon:hover p,
+    .btn-icon:hover .fa {
+        background-color: #c9d41eff;
+        color: #000 !important;
+    }
+
+    .p-btn-icon {
+        font-size: 2rem;
+        color: #fff !important;
+        text-decoration: none;
+        vertical-align: middle;
+    }
+
+
+    .p-btn-icon a {
+        font-size: 2rem;
+        color: #fff !important;
+        text-decoration: none;
+    }
+
+    .p-btn-icon:hover a {
+        color: #000 !important;
+    }
+
+
+    .fa {
+        margin: 20px;
+        color: #c9d41eff !important;
+    }
+
+    .caption {
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        vertical-align: middle;
+
+
+    }
+
+    .caption h3 {
+        height: 70px;
+        /* color: #4D541F; */
+        font-weight: 700;
+        border-bottom: solid #777 1px;
+        font-size: 2rem;
+    }
+
+    .caption p {
+        height: 80px;
+    }
+
+    .thumbnail>a>img {
+        height: 150px !important;
+
+    }
+
+    .p-pn {
+        padding: 30px;
+        margin: 35px;
+        border: solid;
+        border-color: #4D541F;
+        border-width: 0.1px;
+        border-radius: 5px;
+
+    }
+
+    .p-pn h3,
+    .p-pn a,
+    .p-pn p {
+        color: #080808 !important;
+        font-weight: 700;
+        text-decoration: none !important;
+
+        word-wrap: break-word;
+        overflow: hidden !important;
+        text-overflow: clip !important;
+
+    }
+
+    .p-pn p {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+
+    }
+
+    .termino-box {
+        height: 230px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+        text-align: center;
+    }
+
+    @media(max-width:767px) {}
+
+    @media(min-width:768px) {
+
+        .box-descripcion {
+            height: 140px;
+        }
+
+        .box-imgpeticion {
+            height: 140px;
+        }
+
+    }
+
+    @media(min-width:992px) {}
+
+    @media(min-width:1200px) {}
+</style>
 <div class="container-fluid seccion">
 
     <div class="row">
@@ -666,7 +1018,7 @@
 
         <div class="col-xs-12 col-sm-4">
             <div class="thumbnail bs">
-                <a href="https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/22-08-2022/pm04-rg01_reglamento_interno_para_la_gestion_de_pqrsd_v.1.0_de_08-08-2022.pdf" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/05-01-2023/pm04-rg01_reglamento_interno_para_la_gestion_de_pqrsd_v.2.0_16-12-2022_0.pdf" target="_blank" rel="noopener noreferrer">
                     <div class="caption">
                         <div>
                             <h3>Reglamento Interno de Gestión PQRSD</h3>
@@ -723,357 +1075,4 @@
 </div>
 
 
-<style>
-    .field-item.even h2 {
-        visibility: hidden;
-        margin-top: -50px;
-    }
 
-    .region-content {
-        margin: 0;
-        padding: 15px;
-    }
-
-    /* .col-sm-12 {
-        margin: 0 !important;
-        padding: 0 !important;
-    } */
-
-    .seccion {
-        background-color: #fff;
-        max-width: 940px;
-    }
-
-    .w-100 {
-        max-width: 100%;
-    }
-
-    .w-75 {
-        width: 75%;
-    }
-
-    /* .d-flex {
-        display: flex;
-    } */
-
-    .align-items-center {
-        align-items: center;
-    }
-
-    .justify-content-center {
-        justify-content: center;
-    }
-
-    .rh {
-        margin-top: 15px !important;
-    }
-
-    .h3-xs {
-        font-family: 'Signika', sans-serif;
-        margin: 0px !important;
-        font-size: 5rem;
-        background-image: url('https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/28-08-2021/banner_xs_0.png');
-        background-size: 100%;
-        background-repeat: no-repeat;
-    }
-
-    .h3-xs h1 {
-        padding: 6%;
-        margin: 0px 20px 15px 20px;
-        color: #fff;
-        font-weight: 700;
-        font-size: 5rem;
-
-    }
-
-    .h3-sm {
-        font-family: 'Signika', sans-serif;
-        margin: 0px !important;
-        font-size: 5rem;
-        background-image: url('https://www.movilidadbogota.gov.co/web/sites/default/files/servicios/28-08-2021/banner_lg_0.png');
-        background-size: 100%;
-        background-repeat: no-repeat;
-    }
-
-    .h3-sm h1 {
-        padding: 6%;
-        margin: 0px 20px 15px 20px;
-        color: #fff;
-        font-weight: 700;
-        font-size: 40px;
-
-    }
-
-
-    img {
-        /* margin: 5px; */
-        /* width: 0%; */
-
-    }
-
-    .bs {
-        -webkit-box-shadow: 3px 3px 20px -10px rgba(0, 0, 0, 0.78) !important;
-        box-shadow: 3px 3px 20px -10px rgba(0, 0, 0, 0.78) !important;
-    }
-
-    .panel-body {
-        padding: 10px !important;
-        text-align: center;
-        font-size: 2rem;
-        font-weight: 500;
-        color: #080808 !important;
-        /* text-align: justify; */
-        height: 282px;
-        display: flex;
-        line-height: 1.3 !important;
-    }
-
-    .panel-heading {
-        height: 190px;
-
-    }
-
-    .panel-footer {
-        background-color: #fff !important;
-        height: 160px;
-        display: flex;
-        justify-content: center;
-        border-top: none !important;
-        padding: 0px !important;
-
-    }
-
-    .ph-c1 {
-        text-align: center;
-        background-color: #A12B46 !important;
-        color: #fff !important;
-    }
-
-    .ph-c1 h1 {
-        font-size: 6rem !important;
-        font-weight: 700;
-    }
-
-    .ph-c1 h3 {
-        font-size: 2rem !important;
-        font-weight: 600;
-    }
-
-    .ph-c2 {
-        text-align: center;
-        background-color: #8C90F2 !important;
-        color: #080808 !important;
-    }
-
-    .ph-c2 h1 {
-        font-size: 6rem !important;
-        font-weight: 700;
-    }
-
-    .ph-c2 h3 {
-        font-size: 2rem !important;
-        font-weight: 600;
-    }
-
-    .ph-c3 {
-        text-align: center;
-        background-color: #76cbd0ff !important;
-        color: #080808 !important;
-    }
-
-    .ph-c3 h1 {
-        font-size: 6rem !important;
-        font-weight: 700;
-    }
-
-    .ph-c3 h3 {
-        font-size: 2rem !important;
-        font-weight: 600;
-    }
-
-    .c-verde {
-        background-color: #F0F2D9;
-
-    }
-
-    .c-verde h3 {
-        font-weight: 700;
-    }
-
-    .pc1 {
-        background-color: #A12B46;
-        height: 30px;
-        width: 60px;
-    }
-
-    .pc2 {
-        background-color: #8C90F2;
-        height: 30px;
-        width: 60px;
-    }
-
-    .pc3 {
-        background-color: #76cbd0ff;
-        height: 30px;
-        width: 60px;
-    }
-
-    .c-verde-2 {
-        background-color: #c9d41eff;
-        padding: 15px;
-        color: #080808;
-        margin-left: 15px;
-        margin-right: 15px;
-        margin-top: 25px;
-        text-align: center;
-        display: flex;
-        vertical-align: middle;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .c-verde-2 h3 {
-        font-weight: 700;
-    }
-
-    .btn-icon {
-        height: 280px;
-        padding: 0px 25px 25px 25px;
-        margin: 20px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        text-align: center;
-        vertical-align: middle;
-        background-color: #4D541F;
-        color: #fff;
-        border-radius: 4px;
-        /* transition: 0.5s all; */
-    }
-
-    .btn-icon a {
-        color: #fff;
-        text-decoration: none !important;
-    }
-
-    .btn-icon:hover a {
-        color: #000;
-    }
-
-    .btn-icon:hover,
-    .btn-icon:hover p,
-    .btn-icon:hover .fa {
-        background-color: #c9d41eff;
-        color: #000 !important;
-    }
-
-    .p-btn-icon {
-        font-size: 2rem;
-        color: #fff !important;
-        text-decoration: none;
-        vertical-align: middle;
-    }
-
-
-    .p-btn-icon a {
-        font-size: 2rem;
-        color: #fff !important;
-        text-decoration: none;
-    }
-
-    .p-btn-icon:hover a {
-        color: #000 !important;
-    }
-
-
-    .fa {
-        margin: 20px;
-        color: #c9d41eff !important;
-    }
-
-    .caption {
-        text-align: center;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        vertical-align: middle;
-
-
-    }
-
-    .caption h3 {
-        height: 70px;
-        /* color: #4D541F; */
-        font-weight: 700;
-        border-bottom: solid #777 1px;
-        font-size: 2rem;
-    }
-
-    .caption p {
-        height: 80px;
-    }
-
-    .thumbnail>a>img {
-        height: 150px !important;
-
-    }
-
-    .p-pn {
-        padding: 30px;
-        margin: 35px;
-        border: solid;
-        border-color: #4D541F;
-        border-width: 0.1px;
-        border-radius: 5px;
-
-    }
-
-    .p-pn h3,
-    .p-pn a,
-    .p-pn p {
-        color: #080808 !important;
-        font-weight: 700;
-        text-decoration: none !important;
-
-        word-wrap: break-word;
-        overflow: hidden !important;
-        text-overflow: clip !important;
-
-    }
-
-    .p-pn p {
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-
-    }
-
-    .termino-box {
-        height: 230px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 5px;
-        text-align: center;
-    }
-</style>
-<style>
-    @media(max-width:767px) {}
-
-    @media(min-width:768px) {
-
-        .box-descripcion {
-            height: 140px;
-        }
-
-        .box-imgpeticion {
-            height: 140px;
-        }
-
-    }
-
-    @media(min-width:992px) {}
-
-    @media(min-width:1200px) {}
-</style>
