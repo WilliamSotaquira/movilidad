bf1bc2012e7542a5060ca0b41de2764f725ded66
WilliamSotaquira <william.sotaquira@gmail.com>
2023-03-28 15:31:37 -0500

    Modificación e inclución de los enlaces necesarios para la oferta de las subastas de los vehiculus en abandono descrito en la ley 1730 de 2014

--- resources/views/2021/octubre/abandonados.blade.php
+++ resources/views/2021/octubre/abandonados.blade.php
@@ -67,7 +67,7 @@
             <br>
             <p><strong>Para realizar el registro inicial, debe seguir el siguiente paso a paso.</strong></p>
             <ol>
-                <li>Ingresar al portal <a href="https://www.superbid.com.co/evento/secretaria-de-movilidad-custodia-16-cto-2021-2091-1-1000-am-225329?searchType=all&preOrderBy=orderByFirstOpenedOffers&pageNumber=1&pageSize=30&orderBy=lotNumber:asc;subLotNumber:asc&" target="_blank" rel="noopener noreferrer">www.superbid.com.co</a> y hacer clic en “Registro.</li>
+                <li>Ingresar al portal <a href="https://www.superbid.com.co/evento/secretaria-de-movilidad-custodia-17-cto2021-2091-hora-200pm-228112?searchType=all&preOrderBy=orderByFirstOpenedOffers&pageNumber=1&pageSize=30&orderBy=lotNumber:asc;subLotNumber:asc" target="_blank" rel="noopener noreferrer">www.superbid.com.co</a> y hacer clic en “Registro.</li>
                 <li>Luego debe elegir su contraseña y usuario (login).</li>
                 <li>Lo siguiente es validar su login, pues este le permitirá identificarlo a la hora de realizar sus ofertas</li>
                 <li>Finalmente para terminar el registro, debe leer y aceptar el “Procedimiento de Utilización del Portal Superbid Colombia”, haciendo clic en “CONFIRMAR”, enviando la fotocopia de su cédula y el RUT y 24 horas después tendrá la activación de su registro.</li>
@@ -87,7 +87,7 @@
             <p><strong>Si desea participar en las subastas de la Secretaría Distrital de Movilidad, siga estos pasos:</strong></p>
             <ol>
                 <li>
-                    <a href="https://www.superbid.com.co/evento/secretaria-de-movilidad-custodia-16-cto-2021-2091-1-1000-am-225329?searchType=all&preOrderBy=orderByFirstOpenedOffers&pageNumber=1&pageSize=30&orderBy=lotNumber:asc;subLotNumber:asc&" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.superbid.com.co/evento/secretaria-de-movilidad-custodia-17-cto2021-2091-hora-200pm-228112?searchType=all&preOrderBy=orderByFirstOpenedOffers&pageNumber=1&pageSize=30&orderBy=lotNumber:asc;subLotNumber:asc" target="_blank" rel="noopener noreferrer">
                         <strong>Regístrese e ingrese aquí</strong>
                     </a>
                     para la subasta de vehículos para chatarra o
@@ -120,6 +120,7 @@
         </div>
     </div>
 </div>
+<br>
 
 <!-- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
                                                         Fin Bloque Sección
