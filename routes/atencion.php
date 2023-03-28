a139d7f3963f7be5df756d77a4e2ca42a9325403
WilliamSotaquira <william.sotaquira@gmail.com>
2023-03-28 17:37:00 -0500

    Ajustes en las rutas

---
+++ routes/atencion.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\route;
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------- Tramites y Servicios
+Route::view('/atencion/tramitesServicios/dashboard', 'atencion/tramitesServicios/dashboard');
+
+Route::view('/atencion/tramitesServicios/tramites', 'atencion/tramitesServicios/tramites');
+
+Route::view('/atencion/menu', 'atencion/menu');
+
+Route::view('/atencion/tramitesServicios', 'atencion/tramitesServicios');
+
+Route::view('/atencion/tramites/cursos', 'atencion/tramites/cursos');
+Route::view('/atencion/servicios/cursos', 'atencion/servicios/cursos');
+Route::view('/atencion/principal', 'atencion/principal');
---
+++ routes/entidad.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\route;
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------- Inicio
+Route::view('entidad/sistemasGestion/sdm', 'entidad/sistemasGestion/sdm');
+Route::view('entidad/sistemasGestion/calidad', 'entidad/sistemasGestion/calidad');
