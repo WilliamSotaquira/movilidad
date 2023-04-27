82d0604078f860db8d4df722be9ddac55a8978ec
WilliamSotaquira <william.sotaquira@gmail.com>
2023-04-27 07:46:09 -0500

    Cambio general en la pagina web "Registro de publicaciones"

--- resources/views/2022/10/pubicaciones.blade.php
+++ resources/views/2022/10/pubicaciones.blade.php
@@ -1,48 +1,62 @@
-<hr>
-<p>Listado de documentos publicados en el sitio web relacionados con el cumplimiento de la Ley 1712 de 2014 y el Decreto Reglamentario 103 de 2015.</p>
-<br>
-<!-- collapse_publicaciones -->
-<div class='panel panel-primary'>
-    <div class='panel-heading' role='tab' id='heading_publicaciones'>
-        <h4 class='panel-title'>
-            <a role='button' data-toggle='collapse' data-parent='#accordion' href='#collapse_publicaciones' aria-expanded='true' aria-controls='collapse_publicaciones'>
-                Registro de Publicaciones SDM
-            </a>
-        </h4>
-    </div>
-    <div id='collapse_publicaciones' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_publicaciones'>
-        <div class='panel-body'>
+<div class="container">
+    <hr>
+    <p>Listado de documentos publicados en el sitio web relacionados con el cumplimiento de la Ley 1712 de 2014 y el Decreto Reglamentario 103 de 2015.</p>
 
-        <!-- collapse_registroPublicaciones -->
-<div class='panel-group' id='accordion_registroPublicaciones' role='tablist ' aria-multiselectable='true'>
+    <p><br>
 
-<!-- collapse_2022 -->
-<div class='panel panel-primary'>
-    <div class='panel-heading' role='tab' id='heading_2022'>
-        <h4 class='panel-title'>
-            <a role='button' data-toggle='collapse' data-parent='#accordion' href='#collapse_2022' aria-expanded='true' aria-controls='collapse_2022'>
-                2022
-            </a>
-        </h4>
-    </div>
-    <div id='collapse_2022' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_2022'>
-        <div class='panel-body'>
-            <ul>
-                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/01-10-2022/registro_de_publiaciones_-_septiembre_2022.xlsx" target="_blank" rel="noopener noreferrer" download>Septiembre</a></li>
-                <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/01-10-2022/registro_de_publiaciones_-_agosto_2022.xlsx" target="_blank" rel="noopener noreferrer" download>Agosto</a></li>
-                <li><a href="www.movilidadbogota.gov.co/web/sites/default/files/Paginas/22-08-2022/remision_de_informacion_para_pub.xls" target="_blank" rel="noopener noreferrer" download>Julio</a></li>
-            </ul>
+        <!-- collapse_publicaciones -->
+    <div class='panel-group' id='accordion_pub' role='tablist ' aria-multiselectable='true'>
+
+        <!-- collapse 2023 -->
+        <div class='panel panel-primary'>
+            <div class='panel-heading' role='tab' id='heading_2023'>
+                <h4 class='panel-title'>
+                    <a role='button' data-toggle='collapse' data-parent='#accordion_pub' href='#collapse_2023' aria-expanded='false' aria-controls='collapse_2023'>
+                        2023
+                    </a>
+                </h4>
+            </div>
+            <div id='collapse_2023' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='heading_2023' aria-expanded='false'>
+                <div class='panel-body'>
+                    <div class='box-2023'>
+                        <ul>
+                            <li><a download="" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/30-03-2023/remision_de_informacion_para_pub_febrero_2023.xls" rel="noopener noreferrer" target="_blank">Febrero</a></li>
+                            <li><a download="" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/30-03-2023/remision_de_informacion_para_pub_enero_2023.xls" rel="noopener noreferrer" target="_blank">Enero</a></li>
+
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+            <style>
+                .box-2023 {
+                    grid-area: 2023;
+                }
+            </style>
         </div>
-    </div>
-</div>
-<!-- /#fin _2022 -->
 
+        <!-- collapse_2022 -->
+        <div class="panel panel-primary">
+            <div class="panel-heading" id="heading_2022" role="tab">
+                <h4 class="panel-title"><a aria-controls="collapse_2022" aria-expanded="true" data-parent="#accordion" data-toggle="collapse" href="#collapse_2022" role="button">2022 </a></h4>
+            </div>
 
-</div>
-<!-- /#fin _registroPublicaciones -->
-
-
+            <div aria-labelledby="heading_2022" class="panel-collapse collapse in" id="collapse_2022" role="tabpanel">
+                <div class="panel-body">
+                    <ul>
+                        <li><a download="" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/30-03-2023/remision_de_informacion_para_pub_diciembre_2022.xls" rel="noopener noreferrer" target="_blank">Diciembre</a></li>
+                        <li><a download="" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-12-2022/remision_de_informacion_para_2211.xls" rel="noopener noreferrer" target="_blank">Noviembre</a></li>
+                        <li><a download="" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-12-2022/remision_de_informacion_para_2210.xls" rel="noopener noreferrer" target="_blank">Octubre</a></li>
+                        <li><a download="" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-12-2022/remision_de_informacion_para_2209.xls" rel="noopener noreferrer" target="_blank">Septiembre</a></li>
+                        <li><a download="" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-12-2022/remision_de_informacion_para_2208.xls" rel="noopener noreferrer" target="_blank">Agosto</a></li>
+                        <li><a download="" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/26-12-2022/remision_de_informacion_para_2207.xls" rel="noopener noreferrer" target="_blank">Julio</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
+        <!-- /#fin _2022 -->
+
     </div>
+    <!-- /#fin _publicaciones -->
+    <!-- collapse_publicaciones --></p>
 </div>
-<!-- /#fin _publicaciones -->
