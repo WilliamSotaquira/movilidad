11975d9d1724a35e3f1c0fb7a082664afc76f685
WilliamSotaquira <william.sotaquira@gmail.com>
2023-06-27 17:50:34 -0500

    Actualización del recurso descargable con el organigrama de la SDM para junio de 2023 y Adición del perfil del organigrama de Andrea Ramírez Suárez a la Subdirección de Control e Investigaciones al Transporte Público

--- resources/views/organigrama/consolidado.blade.php
+++ resources/views/organigrama/consolidado.blade.php
@@ -13,7 +13,7 @@
     <div class="row">
         <div class="col-xs-12 text-center">
             <div class="boton_org">
-                <a class="btn-org" href="" target="_blank" rel="noopener noreferrer">Ver Organigrama Completo</a>
+                <a class="btn-org" href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-06-2023/organigrama_20230627.pdf" target="_blank" rel="noopener noreferrer">Ver Organigrama Completo</a>
             </div>
         </div>
     </div>
@@ -1603,22 +1603,21 @@
                                                                     <div class="row">
                                                                         <div class="col-xs-12 col-sm-4 text-center">
                                                                             <div class="img-perfil w-100">
-                                                                                <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/23-08-2022/miguelantoniocapadorsanchez.png" alt="foto Miguel Antonio Capador Sánchez">
+                                                                                <img class="img-circle" src="https://placehold.co/400x400?text=Andrea%20Ram%C3%ADrez%20Su%C3%A1rez" alt="foto Andrea Ramírez Suárez">
                                                                             </div>
                                                                         </div>
                                                                         <div class="col-xs-12 col-sm-8 ">
                                                                             <div class="descripcion">
-                                                                                <h1>Miguel Antonio Capador Sánchez</h1>
-                                                                                <h4>Subdirector de Control e Investigaciones al Transporte Público</h4>
+                                                                                <h1>Andrea Ramírez Suárez</h1>
+                                                                                <h4>Subdirección de Control e Investigaciones al Transporte Público</h4>
                                                                                 <div class="descripcion-xs">
-                                                                                    <p>Abogado con especialización en Derecho Procesal y estudios de Maestría en Derecho Penal y Criminología de la Universidad Libre de Colombia.</p>
-                                                                                    <p>Amplia experiencia y conocimiento en manejo de grupos de trabajo, capacidad para desarrollar líneas de investigación y mejoramiento en la calidad personal y académica. Se ha desempeñado como docente en la cátedra profesional con destrezas en procesos de participación ciudadana y democrática.</p>
-                                                                                    <p>Laboró en organizaciones como la Defensoría del Pueblo, el Congreso de la República de Colombia, la Procuraduría General de la Nación y ha trabajado como profesor en universidades como: Universidad Cooperativa de Colombia, Corporación Unificada Nacional de Educación Superior (CUN) y la Universidad Libre de Colombia.</p>
-                                                                                    <p>Ejerce su profesión como litigante y se describe como penalista por convicción. Se siente feliz y agradecido con Dios por hacer parte del equipo de la Secretaría de Movilidad, y manifiesta su compromiso con la mejora continua para brindarle un excelente servicio a los ciudadanos desde esta subdirección.</p>
+                                                                                    <!-- <p>Abogado con especialización en Derecho Procesal y estudios de Maestría en Derecho Penal y Criminología de la Universidad Libre de Colombia.</p> -->
+                                                                                    <!-- <p>Amplia experiencia y conocimiento en manejo de grupos de trabajo, capacidad para desarrollar líneas de investigación y mejoramiento en la calidad personal y académica. Se ha desempeñado como docente en la cátedra profesional con destrezas en procesos de participación ciudadana y democrática.</p> -->
                                                                                 </div>
                                                                             </div>
                                                                         </div>
                                                                     </div>
+
 
 
                                                                 </div>
---
+++ resources/views/organigrama/perfiles/Andrea Ramírez Suárez.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100">
+            <img class="img-circle" src="https://placehold.co/400x400?text=Andrea%20Ram%C3%ADrez%20Su%C3%A1rez" alt="foto Andrea Ramírez Suárez">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Andrea Ramírez Suárez</h1>
+            <h4>Subdirección de Control e Investigaciones al Transporte Público</h4>
+            <div class="descripcion-xs">
+                <!-- <p>Abogado con especialización en Derecho Procesal y estudios de Maestría en Derecho Penal y Criminología de la Universidad Libre de Colombia.</p> -->
+                <!-- <p>Amplia experiencia y conocimiento en manejo de grupos de trabajo, capacidad para desarrollar líneas de investigación y mejoramiento en la calidad personal y académica. Se ha desempeñado como docente en la cátedra profesional con destrezas en procesos de participación ciudadana y democrática.</p> -->
+            </div>
+        </div>
+    </div>
+</div>
