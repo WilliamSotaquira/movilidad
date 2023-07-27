0a7e6f125db619b4455e0c5f7bda42d005f36b6f
WilliamSotaquira <william.sotaquira@gmail.com>
2023-07-27 15:29:18 -0500

    Ajustes a las rutas de navegación

---
+++ resources/views/Transparencia/participacion.blade.php
@@ -0,0 +1,309 @@
+@extends('welcome')
+@section('title','participacion')
+@section('contenido')
+
+<!-- Set participacion -->
+<div class="set-wrapper participacion">
+
+    <div class="container-fluid participacion">
+        <div class="box-wrp">
+            <div class="box-head">
+                <h3 class="text-center"><strong>Participación Ciudadana</strong></h3>
+            </div>
+
+            <div class="box-definicion">
+                <p style="text-indent:0px; -webkit-text-stroke-width:0px; text-align:justify; margin-bottom:10px"><span>La participación ciudadana es una condición de primer orden para una gestión pública íntegra y transparente, por lo tanto, la Secretaría Distrital de Movilidad (SDM) procura constantemente que sus acciones estén dotadas de valor público, es decir, que su quehacer institucional sea percibido por la ciudadanía en general como un ejercicio que genera cambios positivos en el bienestar social del Distrito. Para que esto sea posible, especialmente en un sector de tan alta complejidad como la movilidad en una ciudad como Bogotá, se requiere de la generación de mecanismos de confianza entre la ciudadanía y la institución pública, por medio de un ejercicio sistemático de comunicación y participación ciudadana.</span></p>
+
+                <p><span>Es por ello que la&nbsp;Secretaría Distrital de Movilidad (SDM) cuenta con un Plan Institucional de Participación Ciudadana el cual tiene como objetivo&nbsp;promover la incidencia de la participación ciudadana en las acciones, proyectos, programas, planes y políticas de Movilidad, orientadas al desarrollo de iniciativas de movilidad segura, accesible, incluyente, sostenible y multimodal en el Distrito Capital.</span></p>
+            </div>
+
+            <div class="box-estrategicos">
+                <h4 class="text-center"><strong>Ejes Estratégicos del Plan Institucional de Participación</strong></h4>
+                &nbsp;
+
+                <ul>
+                    <li>
+                        <p><strong>EJE 1. GESTIÓN DEL CONOCIMIENTO</strong> Aportar herramientas y conocimientos a la ciudadanía, incentivando su participación de manera incidente, viable y efectiva en las políticas, programas y proyectos liderados por la Secretaría Distrital de Movilidad.</p>
+                    </li>
+                    <li>
+                        <p><strong>EJE 2: MEDIACIÓN COMUNITARIA: GESTIÓN DE CONFLICTOS, RIESGOS SOCIALES Y HECHOS VICTIMIZANTES</strong> Gestionar los conflictos ocasionados por temas de movilidad, o que la afecten, así como los impactos negativos de la implementación, operaciones e infraestructura vial, y los hechos victimizantes de los siniestros viales identificados en el Distrito.</p>
+                    </li>
+                    <li>
+                        <p><strong>EJE 3: GESTIÓN SOCIAL DE PROYECTOS</strong> Generar acciones que garanticen la inclusión de los procesos de gestión social en las fases de desarrollo del ciclo de programas y proyectos de tránsito, transporte y movilidad, operación e infraestructura vial del servicio de transporte público de pasajeros y transporte no motorizado a cargo de la SDM.</p>
+                    </li>
+                    <li>
+                        <p><strong>EJE 4: RENDICIÓN DE CUENTAS</strong> Promover la transparencia de la gestión de la administración pública y lograr la adopción de los principios de Buen Gobierno, eficiencia, eficacia, transparencia y rendición de cuentas, en la cotidianidad de la gestión pública de la Secretaría Distrital de Movilidad.</p>
+                    </li>
+                    <li>
+                        <p><strong>EJE 5: GESTIÓN SOCIAL DE LAS POLÍTICAS PÚBLICAS</strong> Incorporar los enfoques diferenciales, poblacionales, territoriales y de género en los procesos participativos, así como en los planes, programas y proyectos de la Secretaría Distrital de Movilidad, desde una perspectiva incluyente e intersectorial.</p>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="box-botonPIP">
+                <div class="pip"><a href="https://www.movilidadbogota.gov.co/web/transparencia/plan_institucional_de_participacion" rel="noopener noreferrer" target="_blank"><img alt="Portada de el Plan Institucional de Participación" class="zoom img-responsive w-100" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/06-06-2023/portada_pip_2023.png" title="De clic aqui para ir al historico de Plan Institucional De Participación – PIP"> </a></div>
+
+                <div class="parrafo-pip">
+                    <div>
+                        <div class="imagen-clm"><a href="https://www.movilidadbogota.gov.co/web/centros-locales-de-movilidad" rel="noopener noreferrer" target="_blank"><img alt="Centros Locales de Movilidad" class="img-responsive w-100" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/07-07-2022/clm_ninos_logo_0.png" title="Centros Locales de Movilidad"> </a></div>
+                    </div>
+
+                    <div>
+                        <h4>Aquí podrás acceder al Plan Institucional De Participación</h4>
+                    </div>
+
+                    <div class="btn-pip"><a href="https://www.movilidadbogota.gov.co/web/transparencia/plan_institucional_de_participacion" rel="noopener noreferrer" style="color: #fff" target="_blank">Ingrese aquí </a></div>
+                </div>
+            </div>
+
+            <div>
+                <hr>
+            </div>
+
+            <div class="box-botones ">
+                <div class="boton-clm zoom featured">
+                    <h4><a href="https://www.movilidadbogota.gov.co/web/rendicion_de_cuentas_locales" rel="noopener noreferrer" target="_blank">Rendición de Cuentas Locales</a></h4>
+                </div>
+
+                <div class="boton-clm zoom">
+                    <h4><a href="https://www.movilidadbogota.gov.co/web/centros-locales-de-movilidad" rel="noopener noreferrer" target="_blank">Centros Locales de Movilidad</a></h4>
+                </div>
+
+                <div class="boton-clm zoom">
+                    <h4><a href="https://www.movilidadbogota.gov.co/web/agendas_participativas" rel="noopener noreferrer" target="_blank">Informes y seguimiento de agendas participativas</a></h4>
+                </div>
+
+                <div class="boton-clm zoom">
+                    <h4><a href="https://www.movilidadbogota.gov.co/web/barrios_vitales" rel="noopener noreferrer" target="_blank">Barrios Vitales</a></h4>
+                </div>
+
+                <div class="boton-clm zoom">
+                    <h4><a href="https://www.movilidadbogota.gov.co/web/ferias_de_servicio_movil" rel="noopener noreferrer" target="_blank">Ferias de Servicio Móvil</a></h4>
+                </div>
+
+                <div class="boton-clm zoom featured">
+                    <h4><a href="https://www.movilidadbogota.gov.co/web/grupos_de_valor_partes_interesadas_sdm" rel="noopener noreferrer" target="_blank">Grupos de Valor - Partes Interesadas</a></h4>
+                </div>
+
+
+            </a>
+            </div>
+        </div>
+    </div>
+
+    <p>&nbsp;</p>
+
+    <p><br>
+        <!-- End Set Wrapper -->
+        <style type="text/css">
+            .box-wrp {
+                padding: 16px;
+                display: grid;
+                gap: 20px;
+                grid-auto-flow: dense;
+                grid-template-columns: 1fr;
+                grid-template-rows: auto;
+                grid-template-areas:
+                    '';
+            }
+
+            .box-wrp ul>li {
+                padding-top: 16px;
+            }
+
+            .box-botonPIP {
+                display: grid;
+                /* gap: 10px; */
+                grid-auto-flow: dense;
+                grid-template-columns: 1fr 1fr;
+                grid-template-rows: auto;
+
+            }
+
+            .box-botonPIP .pip {
+                /* height: px; */
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .box-botonPIP .pip img {
+                width: 60%;
+                margin: auto;
+                border: solid 1px #d5d5d5;
+                border-radius: 6px;
+                -webkit-box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.16);
+                box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.16);
+            }
+
+            .parrafo-pip {
+                /* height: px; */
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                text-align: center;
+            }
+
+            .parrafo-pip img {
+                max-width: 250px;
+                margin: auto;
+                padding: 20px;
+            }
+
+            .parrafo-pip h4 {
+                /* font-size: 1em; */
+                font-weight: 700;
+                text-align: center;
+                line-height: 1.3;
+                color: rgba(25, 25, 25, 1);
+                margin: 8px;
+            }
+
+            .parrafo-pip .btn-pip {
+                background-color: rgba(77, 84, 31, 1);
+                color: rgba(255, 255, 255, 1);
+                padding: 8px 16px;
+                margin: 20px;
+                border-radius: 3px;
+            }
+
+            .parrafo-pip .btn-pip:hover {
+                background-color: #A6B517;
+                color: rgba(25, 25, 25, 1);
+
+            }
+
+            .parrafo-pip a .btn-pip {
+                color: rgba(255, 255, 255, 1);
+                text-decoration: none !important;
+            }
+
+            .parrafo-pip a .btn-pip:hover {
+                color: rgba(25, 25, 25, 1);
+            }
+
+            .box-botones {
+                display: grid;
+                gap: 20px;
+                grid-auto-flow: dense;
+                grid-template-columns: repeat(auto-fill, minmax(min(100%, 200px), 1fr));
+                grid-auto-rows: 90px;
+                grid-template-areas:
+                    '';
+            }
+
+            .box-botones a {
+                text-decoration: none !important;
+                font-size: 16px;
+                font-weight: 600;
+                text-align: center;
+                line-height: 1.1;
+                color: rgba(255, 255, 255, 1);
+
+            }
+
+            .box-botones .boton-clm {
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                text-decoration: none !important;
+                border-radius: 4px;
+                -webkit-box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.16);
+                box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.16);
+            }
+
+            .box-botones .boton-clm {
+                /* margin: 20px; */
+                padding: 10px 8px;
+                background-color: rgba(77, 84, 31, 1);
+            }
+
+            .box-botones .boton-clm:hover {
+                background-color: #A6B517;
+            }
+            .box-botones .boton-clm:hover a{
+                color: #303030;
+            }
+
+            .box-botones .boton-clm h4 {
+                font-size: 16px;
+                font-weight: 600;
+                text-align: center;
+                line-height: 1.1;
+                color: rgba(255, 255, 255, 1);
+                margin: 8px;
+            }
+
+            .box-botones .boton-clm:hover h4 {
+                color: rgba(25, 25, 25, 1);
+            }
+        </style>
+        <!-- Styles -->
+        <style type="text/css">
+            .field-item.even h2 {
+                visibility: hidden;
+                margin-top: -50px;
+            }
+
+            .featured {
+                grid-column: span 2;
+            }
+
+            .region-content {
+                padding: 0px;
+                overflow: hidden;
+            }
+
+            .participacion {
+                max-width: 980px;
+                word-wrap: break-word !important;
+                font-family: 'Montserrat', sans-serif;
+                font-size: 15px;
+                font-weight: 500;
+                text-align: justify;
+                line-height: 1.4;
+                color: rgba(25, 25, 25, 1);
+                margin: auto;
+                padding: 8px;
+            }
+
+            .zoom {
+                transition: 0.5s ease;
+                -moz-transition: 0.5s ease;
+                /* Firefox */
+                -webkit-transition: 0.5s ease;
+                /* Chrome - Safari */
+                -o-transition: 0.5s ease;
+                /* Opera */
+                -ms-transition: 0.5s ease;
+            }
+
+            .zoom:hover {
+                transform: scale(1.02);
+                -moz-transform: scale(1.02);
+                /* Firefox */
+                -webkit-transform: scale(1.02);
+                /* Chrome - Safari */
+                -o-transform: scale(1.02);
+                /* Opera */
+                -ms-transform: scale(1.02);
+            }
+
+            /* IE9 */
+
+            .w-100 {
+                width: 100%;
+            }
+
+        </style>
+        <!-- End Styles -->
+    </p>
+
+    <p>&nbsp;</p>
+
+</div>
+<!-- Fin Set -->
---
+++ resources/views/Transparencia/pie.blade.php
@@ -0,0 +1,132 @@
+@extends('welcome')
+@section('title','estrategicos')
+@section('contenido')
+
+<!-- Set estrategicos -->
+<div class="set-wrapper estrategicos">
+
+    <p>&nbsp;</p>
+    <p>Teniendo en cuenta la implementación del Modelo Integrado de Planeación y Gestión y su relación con Plan de Acción de que trata el artículo 74 de la Ley 1474 de 2011, se integran los planes institucionales y estratégicos que se relacionan a continuación y su respectiva publicación a más tardar el 31 de enero de cada año.</p>
+    <p>Con el fin de facilitar la participación de la ciudadanía capitalina, se habilita este botón para recibir observaciones, aportes y/o recomendaciones frente a la planeación institucional de la Secretaria Distrital de Movilidad.</p>
+    <p>&nbsp;</p>
+
+    <hr>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+            <h3><a class="btn btn-primary btn-block" href="https://forms.gle/Lt695k5DfbTqeg5L8">Clic aquí y participa con tus aportes</a></h3>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+            <h3><a class="btn btn-primary btn-block" href="https://docs.google.com/forms/d/e/1FAIpQLSfLL1mmByjzbD1o7G-DvpfESIgARGT_lCRVjBftgFg5jNgrRQ/viewform?usp=sf_link">Evaluación de la documentación de la Secretaría Distrital de Movilidad</a></h3>
+        </div>
+    </div>
+    <hr>
+    <p>&nbsp;</p>
+    <h3 class="text-center">Planes del Decreto 612 de 2018 vigencia 2023</h3>
+    <div class="well well-lg">
+        <ol>
+            <li><strong><a href="https://www.movilidadbogota.gov.co/web/Plan_contra_corrupcion">PLAN ANTICORRUPCIÓN Y DE ATENCIÓN AL CIUDADANO PAAC SDM 2023</a></strong>
+                <ol>
+                    <li value="1,1"><a href="https://www.movilidadbogota.gov.co/web/gestion_de_riesgos" rel="noopener noreferrer" target="_blank">MAPA DE RIESGOS DE CORRUPCIÓN SDM 2023</a></li>
+
+                </ol>
+            </li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/07-02-2023/1._pa01-pl04_plan_institucional_de_archivos_pinar_2023.pdf">PA01-PL04 Plan Institucional de Archivos PINAR 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/plan_anual_de_adquisiciones">Plan Anual de Adquisiciones SDM 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/3._pa02-pl04plan_de_vacantes_2023_v_1.0_0.pdf">PA02-PL04 Plan Anual de Vacantes SDM 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/4._pa02-pl05_plan_de_prevision_de_recursso_humanos_2023.pdf">PA02-PL05 Plan de Previsión de Recursos Humanos SDM 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/5._pa02-pl03_plan_estrategico_del_th_2023_v_1.0_0.pdf">PA02-PL03 Plan Estratégico de Talento Humano SDM 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/6._pa02-pl01_plan_institucional_de_capacitacion_v_1.0.pdf">PA02-PL01 Plan Institucional de Capacitación PIC 2023-2024</a>
+                <ol>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/6.1_plan_de_accion_pic_2023_v1.0_0.xlsx">Plan de Acción Plan Institucional de Capacitación SDM 2023</a></li>
+                </ol>
+            </li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/7._pa02-pl06_plan_de_bienestar_social_e_incentivos_2023_v1.0_0.pdf">PA02-PL06 Plan de Bienestar Social e Incentivos SDM 2023</a>
+                <ol>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/7.1_pa02-pl06-f01_cronograma_plan_de_trabajo_anual_del_bienestar_2023_0.xlsx">Cronograma Plan de bienestar Social e incentivos SDM 2023</a></li>
+                </ol>
+            </li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/15-02-2023/8._pa02-pl07_plan_de_trabajo_anual_del_sst_sdm_2023.pdf">PA02-PL07 Plan de Trabajo Anual del SST SDM 2023</a>
+                <ol>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/8.1_cronograma_plan_de_trabajo_anual_del_sst_2023_1.xlsx">Cronograma Plan de Trabajo Anual del SST 2023</a></li>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/8.2_plan_de_capacitacion_plan_de_trabajo_sst_2023.xlsx">Plan de Capacitación Plan de Trabajo SST 2023</a></li>
+                </ol>
+            </li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/9._plan_estrategico_de_las_tecnologias_de_la_informacion_y_comunicaciones_peti_2020-2024.pdf">PA04-PL01 Plan Estratégico de las Tecnologías de la Información y Comunicaciones - PETI 2020-2024</a>
+                <ol>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/9.1_pa04-pl02_plan_estrategico_de_tecnologias_de_la_informacion_peti_2023_1.pdf">PA04-PL02 Plan Estratégico de Tecnologías de la Información PETI 2023</a></li>
+                </ol>
+            </li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/10._pa04-pl05_plan_de_tratamiento_de_riesgos_de_seguridad_de_la_informacion_2023_1_0.pdf">PA04-PL05 Plan de Tratamiento de Riesgos de Seguridad de la Información 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/11._pa04-pl03_plan_estrategico_de_seguridad_de_la_informacion_pesi_2033_1_0.pdf">PA04-PL03 Plan Estratégico de Seguridad de la Información (PESI) 2023</a></li>
+        </ol>
+    </div>
+
+    <hr>
+    <p>&nbsp;</p>
+
+    <h3 class="text-center">Planes Vigencia 2023</h3>
+
+    <div class="well well-lg">
+        <ol>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/1._pe02-pl01_plan_de_comunicaciones_y_cultura_2023_v1.0_1.pdf">PE02-PL01 Plan de Comunicaciones y Cultura 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/2._plan_de_adecuacion_y_sostenibilidad_mipg_2023_v._0_de_30012023_1_0.xlsx">Plan de Adecuación y Sostenibilidad de MIPG 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/3._pa01-pl01_plan_institucional_del_sistema_integrado_de_conservacion_de_documentos_sic_2023_1.pdf">PA01-PL01 Plan Institucional Sistema Integrado de Conservación Documental SIC 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/4._pa04-pl06_plan_de_mantenimiento_de_servicios_tecnologicos_2023.xlsx">PA04-PL06 Plan de Mantenimiento de Servicios Tecnológicos 2023 </a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/5._plan_de_austeridad_e_indicadores_2023_1_0.xlsx">Plan de austeridad e indicadores 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/6._plan_institucional_de_gestion_ambiental_piga_2020-2024_1.pdf">Plan institucional de gestión ambiental PIGA 2020-2024</a>
+                <ol>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/6.1_pa01-pl03_plan_institucional_de_gestion_ambiental_piga_2023.pdf">PA01-PL03 Plan Institucional de Gestión ambiental PIGA 2023</a></li>
+                </ol>
+            </li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/31-01-2023/7._pa04-pl04_plan_de_datos_abiertos_2023_1.pdf">PA04-PL04 Plan de Datos Abiertos 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/06-03-2023/plan_institucional_de_participacion_ciudadana_sdm_2023_version_2.0.pdf">PM06-PL01 Plan Institucional de Participación Ciudadana SDM 2023</a>
+                <ol>
+                    <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/07-07-2023/8.1_pm06-pl01-f01_cronograma_de_actividades_de_participacion_ciudadana_sdm_2023.xlsx">PM06-PL01-F01 Cronograma de actividades de Participación Ciudadana SDM 2023</a></li>
+                </ol>
+            </li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/12-01-2023/9._plan_cuatrienal_de_gestion_estrategica_del_talento_humano.pdf">Plan Cuatrienal de Gestión Estratégica del Talento Humano 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/planes_de_accion_anual">Planes Operativos Anuales 2023</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/proyectos_de_inversion_2020_2024">Proyectos de Inversión 2020 - 2024</a></li>
+        </ol>
+    </div>
+
+    <hr>
+    <p>&nbsp;</p>
+
+    <h3 class="text-center">Planes Institucionales y Estratégicos Históricos</h3>
+
+    <div class="well well-lg">
+        <ul>
+            <li><a href="https://www.movilidadbogota.gov.co/web/planes_institucionales_y_estrategicos_vigencia_2022">Planes Institucionales y Estratégicos Vigencia 2022</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/planes_institucionales_y_estrategicos_vigencia_2021">Planes Institucionales y Estratégicos Vigencia 2021</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/planes_institucionales_y_estrategicos_vigencia_2020">Planes Institucionales y Estratégicos Vigencia 2020</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/planes_institucionales_y_estrategicos_vigencia_2019">Planes Institucionales y Estratégicos Vigencia 2019</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/planes_institucionales_y_estrat%C3%A9gicos_vigencia_2018_decreto_61218">Planes Institucionales y Estratégicos Vigencia 2018</a></li>
+        </ul>
+    </div>
+
+
+</div>
+<!-- Fin Set -->
+
+<style>
+    .estrategicos ol {
+        counter-reset: contador_item;
+        list-style: none;
+    }
+
+    .estrategicos ol>li {
+        counter-increment: contador_item;
+    }
+
+    .estrategicos ol>li::before {
+        content: counters(contador_item, '.')'.';
+        font-weight: bold;
+        margin-right: 12px;
+        display: inline-block;
+
+    }
+</style>
