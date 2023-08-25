fa801b699aaa959ec326b4668b006c98ca0a38bb
WilliamSotaquira <william.sotaquira@gmail.com>
2023-08-25 14:25:16 -0500

    Desarrollo especifico tipo informativo con nombre "Carta de Integridad"

---
+++ resources/views/2023/08/cartaIntegridad.blade.php
@@ -0,0 +1,133 @@
+@extends('welcome')
+@section('title','cartaIntegridad')
+@section('contenido')
+
+<p style="color:#ffffff; font-size:8px;">Bogotá, agosto de 2023, Apreciada ciudadanía: Hoy, desde el corazón de un equipo compuesto por más de tres mil personas de la Secretaría Distrital de Movilidad,&nbsp;queremos compartirles los grandes retos en estos cuatro años de administración, los cuales hemos asumido con amor, profesionalismo, tenacidad y compromiso las veinticuatro horas del día, los siete días de la semana.&nbsp;</p>
+
+
+<!-- Google fonts - Montserrat  -->
+<link rel='preconnect' href='https://fonts.googleapis.com'>
+<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+<link href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' rel='stylesheet'>
+
+
+
+<!-- Set cartaIntegridad -->
+<div class="set-wrapper cartaIntegridad">
+
+
+    <!-- Sección box header -->
+    <div class="box box-header">
+        <picture>
+            <source media="(min-width: 650px)" srcset="https://www.movilidadbogota.gov.co/web/sites/default/files/Noticias/17-08-2023/g1516.png">
+            <source media="(min-width: 465px)" srcset="https://www.movilidadbogota.gov.co/web/sites/default/files/Noticias/17-08-2023/g2360.png">
+            <img class="" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Noticias/17-08-2023/g2360.png" alt="Encabezado">
+    </div>
+    </picture>
+    <style>
+        .field-item.even .centertil {
+            visibility: hidden;
+            margin-top: -50px;
+        }
+
+        .set-wrapper .box-header {
+            grid-area: box-header;
+            height: 200px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 4em;
+        }
+
+        .box-header img {
+            max-height: 200px;
+            margin: auto;
+        }
+    </style>
+
+
+    <!-- Sección box body -->
+    <div class="box box-body">
+        <h4 class="title title-type-1 title-id-1">Bogotá, agosto de 2023,</h4>
+        <h4 class="title title-type-2 title-id-1">Apreciadas bogotanas y bogotanos, habitantes y visitantes:</h4>
+        <p>&nbsp;</p>
+        <p>Hoy, desde el corazón de un equipo compuesto por más de tres mil personas de la Secretaría Distrital de Movilidad, queremos compartirles los grandes retos en estos cuatro años de administración, los cuales hemos asumido con amor, profesionalismo, tenacidad y compromiso las veinticuatro horas del día, los siete días de la semana. <strong>Sin duda, el desafío más complejo ha sido enfrentar la pandemia del COVID-19, la cual puso a prueba a todos los sectores, en especial el sector de la movilidad,</strong> impulsándonos a modificar los hábitos de desplazamiento y adoptando diversas estrategias con el objetivo de asegurar la movilidad de la población esencial. Nuestra meta fue clara: garantizar que la ciudad no se detuviera en medio de este panorama sin precedentes.</p>
+        <p>Una de las principales estrategias, aplaudida internacionalmente, fue la de implementar kilómetros de ciclorrutas temporales para evitar aglomeraciones en el transporte público. Adicionalmente, se impulsó el distanciamiento en este medio con medidas de bioseguridad. <strong>Incluso durante esta difícil etapa, se aceleraron proyectos cruciales como la puesta en marcha de una gran flota eléctrica,</strong> la cual fue financiada por esta administración, y la finalización de la operación del transporte provisional, que permitió la salida de más cuatro mil buses contaminantes que ya habían cumplido su vida útil, logrando así la completa integración del Sistema de Transporte Público de Bogotá.</p>
+        <p><strong>También nos enfocamos en saldar la deuda histórica de propulsar a Bogotá y la región metropolitana a ser más competitivas con grandes obras de infraestructura,</strong> la primera Línea Metro, Av. Guayacanes, Av. 68, Av Tintal y Extensión de la Caracas, entre otras, hacen parte del gran plan de infraestructura. Sabemos que esto ha ocasionado cierres y desvíos que muchas veces generan congestión, y queremos pedirles disculpas por las incomodidades, este gran esfuerzo es necesario para mejorar la calidad de vida de todas las personas que vivimos en Bogotá.</p>
+        <p>Trabajamos continuamente por organizar la movilidad para el acceso a oportunidades y mayor tiempo para el cuidado. <strong>Una muestra de ello es la consolidación de una fuerza de control con 450 Agentes Civiles de Tránsito y Transporte,</strong> quienes día a día contribuyen a mejorar la movilidad, como también lo son las diferentes medidas como contraflujos, reversibles y priorización de rutas escolares, así como de transporte público, en los principales corredores viales de la ciudad, especialmente en horas pico.</p>
+        <p><strong>¡No queremos más muertes en las vías!</strong> Cada fatídica noticia de un siniestro vial nos duele profundamente. En estos cuatro años, nuestra dedicación incansable ha tenido un objetivo: salvar vidas, en razón a ello, Bogotá se convirtió en la primera ciudad de Colombia en adoptar el límite máximo de velocidad, reduciéndolo de 60 km/h a 50 km/h. Esta medida, aunque haya generado descontento en algunos, atiende las recomendaciones de la Organización Mundial de la Salud (OMS) para entornos urbanos y a su vez cumple con la Ley Julián Esteban, evitando que tragedias como la de este campeón se repitan en nuestra ciudad.</p>
+        <strong><p>De igual forma, visitantes y habitantes de nuestra hermosa Bogotá, les pedimos disculpas si en algún momento los detuvo un operativo de control, o si recibieron un comparendo, realmente nos estamos esforzando en reducir los siniestros viales fortaleciendo la gestión de velocidades seguras y la disuasión de conductas de riesgo con acciones pedagógicas, reducción de calzada, mejor señalización y semaforización. Sabemos también que la extensión del horario de Pico y Placa, no fue una decisión que se tomó a la ligera y que ha demostrado ser acertada frente a dos desafíos importantes:</strong> el incremento en los desplazamientos de las personas tras los meses de aislamiento durante la pandemia, y la ejecución del ambicioso plan de obras 2021 y 2025.</p>
+        <p>Queremos seguirles cumpliendo con servicios mejorados y más eficientes, por ello, empleamos recursos tecnológicos innovadores para agilizar los trámites, ofreciéndoles atención de alta calidad. Nuestra Entidad opera a través de canales de comunicación telefónica, presencial, virtual y multicanal, ofreciendo intérprete de lengua de señas colombiana, llamadas virtuales, chat en línea, chatbot (Lucía) y la opción "Te llamamos". <strong>Desde el comienzo de 2023 está en funcionamiento la Línea Única Nacional: <a href="tel:+057018000127425">018000 127425</a>.</strong></p>
+        <p>Asimismo, a partir de marzo de 2022, ampliamos nuestra cobertura a través de la Ventanilla Única de Servicios de Movilidad (VUS), estableciendo 19 puntos de atención en 17 localidades de Bogotá. También inauguramos cuatro nuevos puntos en Suba, Fontibón, Kennedy y Antonio Nariño,<strong> dedicados a la realización de trámites de Cursos Pedagógicos, y cinco puntos de atención para la salida de patios,</strong> complementando el Centro de Servicio de Movilidad en Calle 13 y Paloquemao.</p>
+        <p>En estos cuatro años, la Movilidad de Bogotá ha avanzado significativamente. <strong>Hemos incorporado un enfoque de género y diferencial,</strong> integrándonos al sistema de cuidado e impulsado el sentido de pertenencia y apropiación a través de la cultura ciudadana. Nuestro modelo de ciudad se basa en una movilidad multimodal, segura y sostenible, que atenúa los impactos ambientales, sociales y económicos, alineándose con las metas del Plan de Acción Climática de la Ciudad.</p>
+        <p>A propósito, <strong>¿alguna vez han visto una caravana de niños y niñas pedaleando hacia su colegio?</strong> ¿O grupos de pequeños acompañados por guías caminando hacia sus lugares de estudio? Si aún no han tenido esa oportunidad, permítannos contarles sobre esta hermosa estrategia impulsada por la Secretaría de Movilidad.</p>
+        <p><strong>Se trata de Niñas y Niños Primero,</strong> que contempla los proyectos Al Colegio en Bici, Bici Parceros, Ciempiés y Guardacaminos, con ellos, se busca mejorar el acceso y la permanencia de los estudiantes en el sistema educativo distrital, y al mismo tiempo, fomentar en ellos la movilidad segura y sostenible. De esta manera los más jóvenes tienen la oportunidad de conocer y apropiarse del espacio público, cobrando así sentido de pertenencia por su ciudad.</p>
+        <p>El 2022 ha sido el mejor año en términos de la operación de estos proyectos, alcanzando más de un millón de viajes, <strong>beneficiando a 129 colegios en quince localidades de la ciudad.</strong> Este año fortalecimos la operación en zona rural de Al Colegio en Bici abriendo nuevas rutas de confianza en Ciudad Bolívar y Usme.</p>
+        <p>Continuando con los actores viales más vulnerables, queremos contarles que hemos avanzado en la construcción de la Política Pública del Peatón, <strong>con la cual se buscará que para el 2035,</strong> la ciudad cuente con las condiciones adecuadas de espacio público, seguridad vial, calidad de infraestructura, seguridad personal, calidad urbana y accesibilidad para promover al peatón como el eje estructurador de la movilidad de la ciudad.</p>
+        <p>Otro proyecto de innovación para la movilidad sostenible, que involucra infraestructura, pacificación y cultura ciudadana es <strong>Barrios Vitales.</strong> En esta administración se han priorizado cinco Barrios Vitales en fase de urbanismo Táctico: San Felipe, Las Cruces-San Bernardo El Porvenir, 20 de Julio - San Blas y San Carlos.</p>
+        <p>Barrios Vitales cuenta con múltiples beneficios para las dinámicas de los sectores seleccionados, <strong>resaltando la revitalización y recuperación del espacio público</strong> para el aprovechamiento y disfrute de los habitantes y las actividades de reactivación, garantizando que las personas puedan desempeñar sus actividades cotidianas con garantías de movilidad en la proximidad.</p>
+        <p>Continuamos promoviendo los modos sostenibles con acciones concretas, como la implementación de la Política Pública de la Bicicleta, que busca mejorar las condiciones físicas, socioeconómicas y culturales de la ciudad para el uso y el disfrute de la bicicleta, una iniciativa que aseguró recursos por $2,2 billones para la ejecución de proyectos bici hasta el año 2039. <strong>El robustecimiento de la cicloinfraestructura,</strong> incluyendo La Red Distrital de Cicloparqueaderos que ya cuenta con más de 62.000 cupos en toda la ciudad, la mitad de ellos certificados con los Sellos de Calidad Oro y Plata de la Secretaría de Movilidad; y la constitución, tras casi una década de esfuerzos, del primer Sistema de Bicicletas Compartidas, inclusivo y con enfoque de género, que ha sido un éxito rotundo, logrando a la fecha más de un millón de viajes. Así avanzamos hacia nuestro propósito de convertirnos en la Capital Mundial de la Bici.</p>
+        <p>De conformidad al nuevo Plan de Ordenamiento Territorial (POT) <strong>trabajamos por una Movilidad Verde, que se adapta al cambio climático en una ciudad interconectada y de proximidad.</strong> vamos por el camino correcto, estableciendo las condiciones para que más de 6.500 vehículos de bajas y nulas emisiones estén registrados y para contar con 20 estaciones de carga rápida para 2024. Asimismo, nos hemos comprometido a que en 2040 el 100% de las ventas de automóviles y furgonetas sean libres de emisiones, y para 2035 el 100% del transporte público también lo sea. Para alcanzar estas metas, hemos adoptado la Política Pública de Movilidad de Cero y Bajas Emisiones 2022-2035. Esta política establecerá las bases para que la movilidad eficiente, accesible y competitiva en la ciudad-región sea una realidad.</p>
+        <p><strong>La movilidad es uno de los aspectos más importantes y determinantes en la vida de los seres humanos; permea todas nuestras actividades, necesidades y relaciones.</strong> Es por ello que, quienes trabajamos en las diferentes áreas, misionales o institucionales de la Secretaría Distrital de Movilidad, estamos comprometidos con los más de ocho millones de Bogotanos y Bogotanas, para que en cada uno de sus roles en la vía: peatón, ciclista, conductor, pasajero de transporte público, de vehículo particular, de moto, de transporte de carga, cuente con las condiciones necesarias de seguridad y accesibilidad en un sistema de cuidado.</p>
+        <p>Por tal razón, somos conscientes de los malestares que se generan cuando un semáforo presenta un daño y ocasiona congestión, cuando no se puede acceder al servicio al pico y placa solidario por fallas e intermitencias en la página, cuando no podemos prestarles un servicio en los tiempos que ustedes lo necesitan, estas situaciones, no quisiéramos que se repitieran. <strong>Por eso trabajamos mancomunadamente para suplir las necesidades y ser mejores todos los días.</strong></p>
+        <p>Para este equipo de servidores, es un orgullo y un honor trabajar por la movilidad de la ciudad. Sabemos que son muchos los desafíos y tareas por cumplir en un sector tan complejo y demandante como lo es el tránsito y la movilidad, <strong>así mismo, estamos convencidos que es un sector apasionante que nos permite cumplir sueños y mejorar la calidad de vida de las personas.</strong> Todas las decisiones que tomamos están basadas en criterios técnicos para lograr mayor equidad y planeación del futuro de nuestra ciudad.</p>
+        <p><strong>Seguiremos trabajando y avanzando por la Bogotá que reverdece,</strong> <strong class="str-2">impulsando la movilidad ambientalmente sostenible, cuidadora, segura e incluyente.</strong></p>
+        <p><strong><i>Secretaría Distrital de Movilidad</i></strong></p>
+
+
+    </div>
+    <p>&nbsp;</p>
+    <a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Noticias/18-08-2023/cartas_senda_integridad_20230818_04.pdf" download="download">Descargue aquí el documento en PDF</a>
+    <p>&nbsp;</p>
+
+    <!-- Sección box footer -->
+    <div class="box box-footer"></div>
+    <style>
+        .set-wrapper .box-footer {
+            grid-area: box-footer;
+            border-top: solid 8px #90c324ff;
+            padding: 4px;
+            border-bottom: solid 16px #1c2243ff;
+        }
+
+        .box-footer {}
+    </style>
+
+    <style>
+        .set-wrapper .box-body {
+            grid-area: box-body;
+        }
+    </style>
+
+
+
+</div>
+<style>
+    .set-wrapper .cartaIntegridad {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 16px;
+        font-weight: normal;
+        text-align: left;
+        line-height: 1.4;
+        color: #2f3031ff;
+    }
+
+    .title-type-1 {
+        color: #2f3031ff;
+    }
+
+    .title-type-2 {
+        color: #90c324ff;
+    }
+
+    .box-body p {
+        text-align: justify;
+        padding-bottom: 32px;
+    }
+
+    .str-2 {
+        color: #90c324ff;
+    }
+</style>
+<!-- Fin Set -->
