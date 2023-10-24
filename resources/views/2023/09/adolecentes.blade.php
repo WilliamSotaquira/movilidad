f84ff0fcdcdfb1965de1a8216692281b8ec73374
WilliamSotaquira <william.sotaquira@gmail.com>
2023-10-24 14:53:59 -0500

    Creación de página tipo blog "Información para niñas, niños y adolescentes" de la sección de Tranparencia de la SDM

---
+++ resources/views/2023/09/adolecentes.blade.php
@@ -0,0 +1,38 @@
+@extends('welcome')
+@section('title','adolecentes')
+@section('contenido')
+
+<!-- Set adolecentes -->
+<div class="set-wrapper adolecentes">
+
+    <p>&nbsp;</p>
+    <p>Aquí encontrarán la gestión realizada desde la Secretaría Distrital de Movilidad la cual está basada en la política pública de infancia y adolescencia en materia del reconocimiento y la garantía de los derechos de la niñez, con la finalidad de construir redes de colaboración, participación activa en temas de Movilidad.
+    </p>
+    <p>&nbsp;</p>
+
+    <h4></h4>
+    <blockquote>
+        <ul>
+            <li><a href="https://www.movilidadbogota.gov.co/web/portal-infantil" rel="noopener noreferrer" target="_blank">Portal Infantil (enlace)</a>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-05-2020/1._video_actividades_cimepies_y_acb.mp4">Video actividades CImepiés y ACB (mp4)</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-05-2020/2._video_ciempies_mes_de_la_ninez.mp4">Video Ciempies mes de la niñez (mp4)</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/19-03-2021/2020_matriz_plan_de_accion_-_ppia-semestre_ii_oficial_s_movilidad-2.xlsx">Matriz de Seguimiento Políticas Públicas Poblacionales 2022 (xlsx)</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/05-07-2023/informe-bogota-derechos-infancia-juventud-2020-2023.pdf">Informe de gestión sobre la garantía de derechos de la primera infancia, infancia, adolescencia y juventud 2020 - 2023 (pdf)</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/politicas_publicas_distritales#collapse_polinf_adol" target="_blank" rel="noopener noreferrer">Política Pública de Primera Infancia y Adolescencia</a></li>
+        </ul>
+    </blockquote>
+
+    <!-- Sección box adolecentes -->
+    <div class="box box-adolecentes">
+        <h4>Rendición de cuentas de niños niñas y adolescentes</h4>
+        <blockquote>
+            <ul>
+                <li><a href="https://www.movilidadbogota.gov.co/web/Invitaci%C3%B3n%20Conversatorio%20Ni%C3%B1as,%20Ni%C3%B1os%20y%20Movilidad">Invitación Conversatorio Niñas, Niños y Movilidad 2023 (pdf)</a></li>
+            </ul>
+        </blockquote>
+    </div>
+
+</div>
+
+<!-- Fin Set -->
+
