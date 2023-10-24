3c72a6fa39dcc62cd59be66afc4bac653d360d69
WilliamSotaquira <william.sotaquira@gmail.com>
2023-10-24 14:59:28 -0500

    Creación de sitio web de tipo blog con información asociada a la recategorización de licencias de condición para refugiados y migrantes en el sitio web de la SDM.

---
+++ resources/views/2023/09/recategorizacion.blade.php
@@ -0,0 +1,224 @@
+@extends('welcome')
+@section('title','recategorizacion')
+@section('contenido')
+
+<!-- Set recategorizacion -->
+<div class="set-wrapper recategorizacion">
+
+<link href="https://fonts.googleapis.com" rel="preconnect">
+<link crossorigin="" href="https://fonts.gstatic.com" rel="preconnect">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+<style type="text/css">.container-convocatoria {
+         color: #252525;
+         font-family: 'Montserrat', sans-serif;
+      }
+
+      .container-convocatoria h3 {
+         text-align: center;
+         font-size: 30px;
+         font-weight: 700;
+         text-align: center;
+         line-height: 1;
+         margin: 0 0 10px 0;
+         padding: 20px 0 20px 0;
+      }
+
+      .container-convocatoria h4 {
+         text-align: center;
+         font-size: 20px;
+         font-weight: 700;
+         text-align: center;
+         line-height: 1;
+         margin: 0 0 10px 0;
+         color: #b4c60c;
+         background-color: #435280;
+         padding: 20px;
+         margin-bottom: 20PX;
+      }
+      .container-convocatoria p {
+         font-size: 14px;
+         font-weight: 400;
+         text-align: justify;
+         line-height: 1.5;
+      }
+
+      .container-convocatoria dl {
+         padding: 8px;
+         margin-bottom: 30px;
+      }
+
+      .container-convocatoria dt {
+         font-size: 16px;
+         font-weight: 700;
+         text-align: initial;
+         line-height: 1.5;
+         padding: 8px 15px 8px 15px;
+         color: #435280;
+         background-color: #b4c60c;
+         border-radius: 10px;
+         margin-bottom: 20px;
+
+      }
+
+      .container-convocatoria dd {
+         font-size: 14px;
+         font-weight: 400;
+         text-align: justify;
+         line-height: 1.5;
+         /* padding: 5px; */
+         margin-left: 16px;
+
+      }
+
+      .container-convocatoria ul,
+      .container-convocatoria ol {
+         padding: 10px;
+      }
+
+      .container-convocatoria ul>li,
+      .container-convocatoria ol>li {
+         margin-left: 15px;
+      }
+
+      .container-convocatoria a {
+         text-decoration: underline;
+         color: #435280;
+         ;
+         font-weight: 700;
+      }
+
+      .field-item.even h2 {
+         visibility: hidden;
+         margin-top: -50px;
+      }
+
+      .region-content {
+         padding: 0px;
+         overflow: hidden;
+      }
+
+      .seccion {
+         font-size: 13px !important;
+         line-height: 1.4 !important;
+         max-width: 940px;
+      }
+
+      .w-100 {
+         width: 100%;
+      }
+</style>
+<div class="container-fluid seccion container-convocatoria">
+	<div class="row"><!-- Banner Mobile -->
+		<div class="col-xs-12 visible-xs-12 visible-xs-block"><img alt="Preguntas y respuestas frecuentes - Migrantes" class="img-responsive w-100 " src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/25-09-2023/banner_340x300_recategoriza_licencia.png" title="Preguntas y respuestas frecuentes - Migrantes"></div>
+		<!-- Banner Desktop -->
+
+		<div class="col-sm-12 hidden-xs"><img alt="Preguntas y respuestas frecuentes - Migrantes" class="img-responsive w-100 " src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/25-09-2023/banner_950x300_recategoriza_licencia.png" title="Preguntas y respuestas frecuentes - Migrantes"></div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-12">
+			<h3>Preguntas y respuestas frecuentes - Migrantes</h3>
+
+			<h4>Proyecto de recategorización de licencias de conducción B1 – C1 y C1 a C2 para refugiados y migrantes</h4>
+
+			<p>La Secretaría Distrital de Movilidad, con el apoyo del Banco Interamericano de Desarrollo (BID) y el Gobierno de Canadá, invita a los refugiados y migrantes, interesados en conseguir licencias de conducción C1 y C2, que les permitan conducir vehículos públicos pequeños y medianos en Colombia, a participar en el proyecto de recategorización de licencias de conducción en Bogotá.</p>
+
+			<p>Esta convocatoria se hace en colaboración del Grupo Interagencial de Flujos Migratorios Mixtos - GIFMM.</p>
+
+			<p>La convocatoria estará abierta desde el 25 de septiembre de 2023.</p>
+
+			<dl>
+				<dt>1. ¿Quiénes pueden participar en la convocatoria?</dt>
+				<dd><strong>R. </strong>La convocatoria va dirigida a personas migrantes (mujeres, hombres y personas de géneros diversos), que cumplan los siguientes requisitos:</dd>
+				<dd>
+					<ul>
+						<li>Ser mayor de 18 años.</li>
+						<li>Tener el Permiso de Protección Temporal (PPT) físico o cédula de extranjería, en el caso de las personas de nacionalidad venezolana.</li>
+						<li>Tener el pasaporte o cédula de extranjería para las personas de otras nacionalidades diferentes a la venezolana.</li>
+						<li>Saber conducir, preferiblemente vehículos medianos y grandes.</li>
+						<li>Residir en Bogotá o tener la intención de vivir en la ciudad, por lo menos durante los 12 meses subsiguientes.</li>
+					</ul>
+				</dd>
+			</dl>
+
+			<dl>
+				<dt>2. ¿Cuántas personas migrantes serán beneficiadas por el proyecto?</dt>
+				<dd><strong>R. </strong>Se seleccionarán un total de 120 personas migrantes, como beneficiarias del proyecto de recategorización de licencias.</dd>
+			</dl>
+
+			<dl>
+				<dt>3. ¿Qué apoyos recibiré si soy seleccionado como beneficiario?</dt>
+				<dd><strong>R. </strong>Las personas que sean seleccionadas, en el marco del proyecto, recibirán lo siguiente:</dd>
+				<dd>
+					<ul>
+						<li>Exámenes de aptitud en conducción.</li>
+						<li>Curso de recategorización/expedición de licencia de conducción C1.</li>
+						<li>Exámenes de aptitud en conducción (cada recategorización es un examen).</li>
+						<li>Curso de recategorización de licencia de conducción de C1 a C2.</li>
+						<li>Pago de los derechos ante la Secretaría Distrital de Movilidad para la expedición de las licencias C1 y C2.</li>
+						<li>Una recarga en la tarjeta SITP, equivalente a 20 trayectos por mes, durante el proceso de formación.</li>
+					</ul>
+				</dd>
+			</dl>
+
+			<dl>
+				<dt>4. ¿Con esta convocatoria me conseguirán un empleo como conductor en Bogotá?</dt>
+				<dd><strong>R. </strong>No. El proyecto no garantiza el acceso a una vacante de empleo. La convocatoria se enfoca en apoyar a aquellas personas que deseen conseguir las licencias de conducción C1 y C2, que les permita trabajar como operarias de transporte privado o público en cualquier ciudad del país; sin embargo, una vez se termine el proyecto de recategorización de licencias, se contempla la posibilidad de remitir algunas hojas de vida a operadores del transporte público en Bogotá.</dd>
+			</dl>
+
+			<dl>
+				<dt>5. ¿Cuánto tiempo durará el proyecto?</dt>
+				<dd><strong>R. </strong>El proyecto durará aproximadamente 6 meses. Por tanto, las personas beneficiarias deberán contar con disponibilidad para realizar los cursos y los trámites respectivos durante ese periodo.</dd>
+			</dl>
+
+			<dl>
+				<dt>6. Yo trabajo y tengo poca disponibilidad de tiempo ¿Necesito dedicar muchas horas a los cursos de formación? ¿Debo cumplir un horario para realizar los cursos de recategorización de licencias?</dt>
+				<dd><strong>R. </strong>No. Los horarios de los cursos no son intensivos y son ajustables. Lo importante es que la persona beneficiaria cumpla el número de horas teóricas y prácticas que estipula el Centro de Enseñanza Automovilística para pasar de una licencia B1 a C1 (45 horas) y de una licencia C1 a C2 (45 horas). La persona beneficiaria podrá organizar su horario para tomar las sesiones de aprendizaje, incluso los fines de semana.</dd>
+			</dl>
+
+			<dl>
+				<dt>7. ¿Puedo inscribirme si no tengo el PPT físico o aprobado, pero tengo un certificado en trámite del PPT?</dt>
+				<dd><strong>R. </strong>No. El certificado de trámite del PPT no es un documento válido para participar en esta convocatoria.</dd>
+			</dl>
+
+			<dl>
+				<dt>8. Tengo una licencia de conducción para manejar moto, ¿puedo participar en la convocatoria?</dt>
+				<dd><strong>R. </strong>No. Sólo pueden participar aquellas personas que tengan una licencia de conducción adquirida en su país o en Colombia para manejar automóviles, microbuses, buses o camiones.</dd>
+			</dl>
+
+			<dl>
+				<dt>9. ¿Es necesario tener experiencia como conductor de transporte público o privado para participar en esta convocatoria?</dt>
+				<dd><strong>R. </strong>Si. Es necesario tener experiencia como conductor de transporte, como mínimo privado, en su país o en Colombia.</dd>
+			</dl>
+
+			<dl>
+				<dt>10. ¿La convocatoria sólo aplica para personas que vivan en Bogotá?</dt>
+				<dd><strong>R. </strong>Sí. La convocatoria está dirigida a personas que residan en Bogotá o que quieran radicarse en la ciudad por al menos 6 meses.</dd>
+			</dl>
+
+			<dl>
+				<dt>11. ¿Me pedirán dinero para participar en el proyecto?</dt>
+				<dd><strong>R. </strong>No. La participación en el proyecto es totalmente gratis. Sugerimos denunciar en el siguiente <a href="https://www.movilidadbogota.gov.co/web/formulario_de_denuncias" target="_blank">enlace</a>, si alguna persona inescrupulosa le indica que debe pagar para participar o ser seleccionado en el proyecto.</dd>
+			</dl>
+
+			<dl>
+				<dt>12. ¿Dónde me inscribo?</dt>
+				<dd><strong>R. </strong>Les invitamos a inscribirse en el <a href="https://forms.gle/diwTSPkdkVQFXbGW8" target="_blank">enlace</a>, si cumple con los requisitos expuestos en la pregunta frecuente número 1.</dd>
+			</dl>
+
+			<dl>
+				<dt>13. ¿Qué sigue después de la inscripción?</dt>
+				<dd><strong>R. </strong>Las personas inscritas serán contactadas para verificar el cumplimiento de la documentación requerida (PPT, licencia de conducción adquirida en Venezuela o en Colombia). Verificada la documentación, una vez preseleccionados, ingresarán a un proceso de selección final por parte de expertos del proyecto.</dd>
+			</dl>
+
+			<dl>
+				<dt>14. ¿Cómo sabré si fui seleccionado(a) para el proyecto?</dt>
+				<dd><strong>R. </strong>Las personas beneficiarias serán contactadas por correo o por teléfono. Si no son contactadas en el transcurso de los siguientes 30 días después de la inscripción y el haber pasado por el proceso de selección, se entenderá que no fueron beneficiadas en esta oportunidad.</dd>
+			</dl>
+		</div>
+	</div>
+</div>
+
+
+</div>
+<!-- Fin Set -->
