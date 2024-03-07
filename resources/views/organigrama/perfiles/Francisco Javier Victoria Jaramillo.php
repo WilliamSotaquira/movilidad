f47c3e2e9888f8d01ea1486933b53cb61bae8a99
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-03-07 09:37:31 -0500

    ajustes

---
+++ resources/views/organigrama/perfiles/Francisco Javier Victoria Jaramillo.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100">
+            <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/07-03-2024/francisco_javier_victoria_jaramillo.png" alt="Foto: Francisco Javier Victoria Jaramillo">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Francisco Javier Victoria Jaramillo</h1>
+            <h4>Subdirector de Transporte Privado</h4>
+            <div class="descripcion-xs">
+                <p></p>
+            </div>
+        </div>
+    </div>
+</div>
