234df64c8224f8ac283c5fc5f55c9578ab7c9cc3
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-05-22 15:38:47 -0500

    TWS-0009

---
+++ resources/views/2024/04/participacion.blade.php
@@ -0,0 +1,26 @@
+@extends('welcome')
+@section('title','participacion')
+@section('contenido')
+
+<!-- Set participacion -->
+<div class="set-wrapper participacion">
+    <div class="row">
+        <p>&nbsp;</p>
+        <p style="line-height:1.295; text-align:justify; margin-bottom:11px"><span style="font-size:11pt; font-variant:normal; white-space:pre-wrap"><span style="font-family:Arial"><span style="color:#000000"><span style="font-weight:400"><span style="font-style:normal"><span style="text-decoration:none">La Secretaría Distrital de Movilidad (SDM), consiente de la importancia de la incidencia de la participación ciudadana en las políticas, planes y proyectos de la entidad, desde 2018 cuenta con la asesoría de la Oficina de Gestión Social (OGS), la cual continúa trabajando por involucrar a la ciudadanía en las acciones que dan desarrollo a la misión de la entidad.</span></span></span></span></span></span></p>
+        <p style="line-height:1.295; text-align:justify; margin-bottom:11px"><span style="font-size:11pt; font-variant:normal; white-space:pre-wrap"><span style="font-family:Arial"><span style="color:#000000"><span style="font-weight:400"><span style="font-style:normal"><span style="text-decoration:none">Dentro de sus prioridades, la OGS identifica las problemáticas de la ciudadanía, y con base en ellas, diagnostica las necesidades territoriales de las diferentes localidades de Bogotá. En coherencia cuenta con la “Guía para la implementación de gestión social en los proyectos de movilidad”, la cual puedes consultar a continuación:</span></span></span></span></span></span></p>
+    </div>
+    <div>
+        <ul>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/29-04-2024/diagnostico_participacion_ciudadana_secretaria_distrital_de_movilidad_diciembre_2023.pdf">1. Diagnóstico Participación Ciudadana Secretaría Distrital de Movilidad Diciembre 2023 (.pdf)</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/29-04-2024/diagnostico_sobre_la_implementacion_de_enfoques_en_la_secretaria_distrital_de_movilidad_diciembre_2023.pdf">2. Diagnóstico sobre la implementación de enfoques en la Secretaría Distrital de Movilidad Diciembre 2023 (.pdf)</a></li>
+            <li><a href="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/29-04-2024/participacion_ciudadana_para_la_identificacion_de_problemas_junio_2023.pdf">3. Participación Ciudadana para la identificación de problemas Junio 2023 (.pdf)</a></li>
+        </ul>
+    </div>
+    <p>&nbsp;</p>
+
+
+
+
+</div>
+<!-- Fin Set -->
+
