410048fe9ee994df5884c95a80a828ac7a1ddedb
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-05-22 16:33:21 -0500

    TWS-0019

---
+++ resources/views/organigrama/perfiles/Sergio Alejandro Peña Pedreros.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100">
+            <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/09-05-2024/sergio_alejandro_pena_pedreros.png" alt="Foto: Sergio Alejandro Peña Pedreros">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Sergio Alejandro Peña Pedreros</h1>
+            <h4>Subdirector de Transporte Público</h4>
+            <div class="descripcion-xs">
+                <p>Ingeniero en Transporte y Vías, especialista en Diseño de Vías, Tránsito y Transporte, magíster en Ingeniería y transporte, cuenta con una amplia experiencia en el desarrollo de diferentes proyectos de movilidad, Planes Maestros de Movilidad, estructuración de Sistemas Estratégicos de Transporte Público, estimaciones de demanda en diferentes modos de transporte y formulación de políticas en materia de movilidad. </p>
+                <p>Desde su nuevo rol en la Secretaría Distrital de Movilidad se compromete a  avanzar en el desarrollo de estrategias de transporte público individual de pasajeros, el Sistema Interoperable de Recaudo, el transporte intermunicipal, el Sistema Integrado de Transporte Público (SITP), la implementación del Fondo de Estabilización Tarifaria (FET) y accesibilidad al transporte público.</p>
+            </div>
+        </div>
+    </div>
+</div>
