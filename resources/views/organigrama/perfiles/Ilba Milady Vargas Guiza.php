c05c07f078c40e00da26e3681bfded850bdb08a9
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-05-22 16:33:08 -0500

    TWS-0018

---
+++ resources/views/organigrama/perfiles/Ilba Milady Vargas Guiza.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100">
+            <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/09-05-2024/ilba_milady_vargas_guiza.png" alt="Foto: Ilba Milady Vargas Guiza">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Ilba Milady Vargas Guiza</h1>
+            <h4>Subdirectora Administrativa</h4>
+            <div class="descripcion-xs">
+                <p></p>
+            </div>
+        </div>
+    </div>
+</div>
