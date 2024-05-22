268a564c958c4cfb159a5b009fd5d5209f228303
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-05-22 16:32:57 -0500

    TWS-0017

---
+++ resources/views/organigrama/perfiles/Clemencia Rojas Arias.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100">
+            <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/09-05-2024/clemencia_rojas_arias.png" alt="Foto: Clemencia Rojas Arias">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Clemencia Rojas Arias</h1>
+            <h4>Subsecretaria de Gestión Corporativa</h4>
+            <div class="descripcion-xs">
+                <p>Clemencia Rojas Arias es abogada de la Universidad de Caldas, con especialización en Derecho Administrativo de la misma universidad, especialización en Contratación Estatal y maestría en Derecho Público, ambas de la Universidad Externado de Colombia.</p>
+                <p>Con una experiencia de 29 años en el sector público, a nivel nacional y distrital, ha destacado en roles directivos de gran responsabilidad, demostrando habilidades sólidas para liderar equipos. En su nuevo rol, se enfrenta al desafío de consolidar la gestión institucional hacia una Secretaría de Movilidad más moderna, enfocada en la transparencia en sus procesos y contribuyendo activamente al cumplimiento de sus objetivos.</p>
+            </div>
+        </div>
+    </div>
+</div>
