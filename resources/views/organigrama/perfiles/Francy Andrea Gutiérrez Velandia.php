92a158322c39fe3c2da42fc15567276db3f57853
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-05-27 13:55:52 -0500

    TWS-0038

---
+++ resources/views/organigrama/perfiles/Francy Andrea Gutiérrez Velandia.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100">
+            <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/27-05-2024/andrea_gutierrez.png" alt="Foto: Francy Andrea Gutiérrez Velandia">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Francy Andrea Gutiérrez Velandia</h1>
+            <h4>Subdirectora de señalización</h4>
+            <div class="descripcion-xs">
+                <p>Francy Andrea Gutiérrez Velandia es arquitecta urbanista egresada de la Universidad Católica de Colombia, con especialización en Gestión de Ciudad y Territorio de la Universidad Externado de Colombia.</p>
+                <p>Cuenta con experiencia en el desarrollo de obras de espacio público, desde su diseño inicial hasta su ejecución, interventoría, diseño urbano, investigación, norma urbanística y proyectos de movilidad activa y sostenible con enfoque en urbanismo táctico y participación ciudadana.</p>
+                <p>Con una trayectoria de cuatro años en la Secretaría de Movilidad -  inicialmente en la Subdirección de Infraestructura y posteriormente liderando el proyecto Barrios Vitales en la Dirección de Planeación de Movilidad -  busca desde su nuevo rol identificar oportunidades de transformación en la señalización urbana mediante acciones innovadoras. Su objetivo es fortalecer los proyectos en curso de la Entidad, proyectando a Bogotá como una ciudad que garantice una movilidad segura para todos sus habitantes.</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/organigrama/perfiles/Paola Adriana Corona Miranda.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100"><img alt="Paola Adriana Corona Miranda" class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/28-07-2022/paola_adriana_corona_miranda.png" title="Paola Adriana Corona Miranda"></div>
+    </div>
+
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Paola Adriana Corona Miranda</h1>
+
+            <h4>Directora de Talento Humano</h4>
+
+            <div class="descripcion-xs">
+                <p>Es administradora de empresas y con especialización en Finanzas y Administración Pública de la Universidad Militar Nueva Granada. Es líder de la política de derechos humanos y empresa del sector de comercio, industria y turismo. Líder en temas de responsabilidad social, derechos humanos y manager en la gestión de la conciliación de EFR (Empresa Familiarmente Responsable), de la fundación MAS Familia de España.</p>
+
+                <p>También está certificada como Auditoria de Calidad Seguridad y Salud en el Trabajo y el Medio Ambiente y tiene cursos en gestión de proyectos.</p>
+
+                <p>Por espacio de 10 años estuvo al servicio de la Superintendencia de Industria y Comercio, donde se desempeñó en la Secretaría General, apoyando todos los procesos de talento humano, gestión documental, financiera y recursos físicos, entre otros.</p>
+            </div>
+        </div>
+    </div>
+</div>
