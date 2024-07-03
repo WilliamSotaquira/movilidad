18e110b48062e27ed239f2f07674efacee5143f6
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-07-02 19:46:24 -0500

    TWS-0075

---
+++ resources/views/organigrama/perfiles/Alejandra Rojas Posada.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100">
+            <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/24-06-2024/alejandra_rojas_posada.png" alt="Foto: Alejandra Rojas Posada">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Alejandra Rojas Posada</h1>
+            <h4>Directora de Atención al Ciudadano</h4>
+            <div class="descripcion-xs">
+                <p>Abogada de la Universidad Libre con especialización en Derecho Penal de la Universidad del Rosario. En su trayectoria profesional se ha desempeñado como consultora y asesora de entidades públicas y privadas en asuntos legales, con amplia experiencia en el derecho administrativo y el manejo de relaciones con los clientes. Trabajó en la Superintendencia de Puertos y Transporte, en el bufete del abogado penalista Jaime Granados Peña, además en la Unidad de Restitución de Tierras, y en el Ministerio de Agricultura y Desarrollo Rural, entre otros.</p>
+                <p>En el año 2020, llegó a la Secretaría Distrital de Movilidad con el fin de liderar y orientar los procesos contravencionales como subdirectora de Contravenciones. Enfrentando los retos que trajo la pandemia del covid 19, generó estrategias para virtualizar los procesos y así dar continuidad al servicio que prestaba la Entidad.</p>
+                <p>Desde septiembre del 2020 se desempeña como Directora de Atención al Ciudadano, con el reto liderar a más de 300 personas en su equipo de trabajo, ha logrado desarrollar nuevas estrategias en beneficio de la ciudadanía, como el agendamiento de los servicios para la atención de los trámites, el desarrollo e implementación de la salida virtual para vehículos inmovilizados para 9 infracciones de tránsito.</p>
+                <p>Dentro de sus logros como Directora, en el 2022, la SDM ocupó el 7mo puesto en la mejor atención en las entidades públicas otorgada por la Veeduría Distrital. Ha logrado llevar a cabo 22 subastas de vehículos inmovilizados, descongestionando los patios del Distrito dando aplicación a la Ley 1730. Lideró el proyecto de transición de la Concesión SIM a la Ventanilla única de Servicios, ampliando la cobertura con más puntos de atención y simplificando los trámites para el ciudadano. Su gestión ha llevado a la apertura de 9 salas adicionales para cursos pedagógicos, teniendo actualmente 13 y 4 puntos de atención adicionales para salida de patios presencial. Llevó a cabo con éxito la transición de la entidad al nuevo Centro de Contacto de Movilidad, poniendo al servicio de la ciudadanía diferentes herramientas virtuales de atención como videollamada en lenguaje de señas, chat en línea, el desarrollo de Lucia, la Asistente Virtual, entre otros.</p>
+            </div>
+        </div>
+    </div>
+</div>
