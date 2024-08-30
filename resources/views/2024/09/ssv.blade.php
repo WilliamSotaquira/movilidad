5fd4ee5fa73a1460243d8e7d90b006aacb711c39
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-08-30 10:01:53 -0500

    tws-0191

---
+++ resources/views/2024/09/ssv.blade.php
@@ -0,0 +1,30 @@
+@extends('welcome')
+@section('title','ssv')
+@section('contenido')
+
+<!-- Set ssv -->
+<div class="set-wrapper ssv">
+
+    <style>
+        .field-item.even .centertil {
+            visibility: hidden;
+            margin-top: -50px;
+        }
+    </style>
+
+    <div class="cpnt cpnt-1">
+        <div class="imagen img1">
+            <img class="img-responsive w-100" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/28-08-2024/16_-_banner_general_semana_agosto28_mesa_de_trabajo_1.png" alt="" title="">
+        </div>
+    </div>
+    <hr>
+    <div class="cpnt cpnt-2">
+        <div class="imagen img-2">
+            <img class="img-responsive w-100" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/28-08-2024/16_-_banner_general_semana_agosto28-03.png" alt="" title="">
+        </div>
+    </div>
+
+
+</div>
+<!-- Fin Set -->
+
