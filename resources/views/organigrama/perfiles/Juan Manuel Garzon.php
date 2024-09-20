bf1c2b84a5069e0f8da6b51b89a7b7c24858cf8a
William Sotaquirá <32824568+WilliamSotaquira@users.noreply.github.com>
2024-09-20 18:22:56 -0500

    20242032_234

---
+++ resources/views/organigrama/perfiles/Juan Manuel Garzon.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4 text-center">
+        <div class="img-perfil w-100">
+            <img class="img-circle" src="https://www.movilidadbogota.gov.co/web/sites/default/files/Paginas/20-09-2024/juan_manuel_garzon.png" alt="Foto: Juan Manuel Garzón Monroy">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-8 ">
+        <div class="descripcion">
+            <h1>Juan Manuel Garzón Monroy</h1>
+            <h4>Subdirección de Contravenciones (e)</h4>
+            <div class="descripcion-xs">
+                <p>Abogado y especialista en Derecho Administrativo y Constitucional de la Universidad Católica de Colombia. Magíster en Gobierno y Políticas Públicas de la Universidad Externado de Colombia, tiene una tesis meritoria en seguridad vial y pedagogía.</p>
+                <p>Desde el 2007 ingresó a trabajar a la Secretaría de Movilidad, como contratista, al Centro de Servicios, y en el 2008 empezó a ejercer como autoridad de tránsito; tiene un corto periodo en el 2020 donde se va de la SDM para laborar como inspector de policía. En el año 2021 regresa a la Entidad en carrera administrativa con funciones de autoridad de tránsito. Ahora que está encargado de esta Subdirección, desde este rol, quiere apostarle a la virtualización de procesos, para que la ciudadanía tenga la posibilidad de acceder a salida de vehículos e impugnaciones de comparendos desde la virtualidad.</p>
+            </div>
+        </div>
+    </div>
+</div>
